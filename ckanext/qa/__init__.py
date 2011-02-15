4303265fc4fb3e3696cc03a43d324dd0f852a117
Wayne Witzel III <wwitzel3@gmail.com>
2011-02-15 07:59:14 -0500

    Don't have any static content to go with it. Templates only.

--- ckanext/qa/__init__.py
+++ ckanext/qa/__init__.py
@@ -29,10 +29,7 @@ class QA(SingletonPlugin):
     def update_config(self, config):
         here = os.path.dirname(__file__)
         rootdir = os.path.dirname(os.path.dirname(here))
-        our_public_dir = os.path.join(rootdir, 'public')
         template_dir = os.path.join(rootdir, 'templates')
-        config['extra_public_paths'] = ','.join([our_public_dir,
-                config.get('extra_public_paths', '')])
         config['extra_template_paths'] = ','.join([template_dir,
                 config.get('extra_template_paths', '')])
 
