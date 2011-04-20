e868e39f79b761f374417bb226b4e885cf4d2d0a
David Read <david.read@okfn.org>
2011-04-20 11:52:45 +0200

    [style]: Use improved CKAN way to hide the sidebar.

--- ckanext/qa/controllers/base.py
+++ ckanext/qa/controllers/base.py
@@ -3,8 +3,4 @@ from pylons import c
 from ckan.lib.base import BaseController
 
 class QAController(BaseController):
-    def __call__(self, environ, start_response):
-        # turns off sidebar in DGU theme
-        c.hide_sidebar = True
-        
-        return super(QAController, self).__call__(environ, start_response)
+    pass
