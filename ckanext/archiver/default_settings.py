9dd39bf0ea32f51bac3e614f28ce69276c8955d4
John Glover <glover.john@gmail.com>
2011-10-04 14:51:28 +0100

    add max_content_length to default_settings

--- ckanext/archiver/default_settings.py
+++ ckanext/archiver/default_settings.py
@@ -6,3 +6,6 @@ ARCHIVE_DIR = '/tmp/archive'
 
 # Use this user name when requesting data from ckan
 ARCHIVE_USER = u'okfn_maintenance'
+
+# Max content-length of archived files, larger files will be ignored
+MAX_CONTENT_LENGTH = 500000
