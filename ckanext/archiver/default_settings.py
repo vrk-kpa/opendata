305e94d347c72de6b837b639d0b0d6e7090ccdc8
John Glover <glover.john@gmail.com>
2011-10-04 13:49:48 +0100

    add ckan config and archive user to default settings

--- ckanext/archiver/default_settings.py
+++ ckanext/archiver/default_settings.py
@@ -1,1 +1,8 @@
+# path to ckan config file
+CKAN_CONFIG = '/home/okfn/pyenv/src/ckan/ckan.ini'
+
+# directory to save downloaded files to
 ARCHIVE_DIR = '/tmp/archive'
+
+# Use this user name when requesting data from ckan
+ARCHIVE_USER = u'okfn_maintenance'
