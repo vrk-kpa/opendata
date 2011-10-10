347681637c7c9d28ba1c787bb77da1296a02d13f
John Glover <glover.john@gmail.com>
2011-10-10 11:13:37 +0100

    Change settings to use API key and CKAN URL

--- ckanext/archiver/default_settings.py
+++ ckanext/archiver/default_settings.py
@@ -1,11 +1,12 @@
-# path to ckan config file
-CKAN_CONFIG = '/home/okfn/pyenv/src/ckan/ckan.ini'
+# URL to the CKAN instance
+CKAN_URL = 'http://127.0.0.1:5000'
+
+# API key for the CKAN user that the archiver will authenticate as.
+# This user must be a system administrator
+API_KEY = ''
 
 # directory to save downloaded files to
 ARCHIVE_DIR = '/tmp/archive'
-
-# Use this user name when requesting data from ckan
-ARCHIVE_USER = u'okfn_maintenance'
 
 # Max content-length of archived files, larger files will be ignored
 MAX_CONTENT_LENGTH = 500000
