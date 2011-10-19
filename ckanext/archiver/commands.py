656532c89e35f45e6d01c8d1ff578df758558902
John Glover <glover.john@gmail.com>
2011-10-19 18:22:18 +0100

    fix archiver cron job

--- ckanext/archiver/commands.py
+++ ckanext/archiver/commands.py
@@ -1,16 +1,19 @@
-from paste.script.command import Command
 import tasks
-
+import json
+import requests
+import urlparse
+from pylons import config
+from ckan.lib.cli import CkanCommand
+from ckan.logic import get_action
+from ckan import model
 import logging
 logger = logging.getLogger()
 
-class Archiver(Command):
+class Archiver(CkanCommand):
     """
     Download and save copies of all package resources.
 
-    If we already have a copy of a resource (tested by checking the hash value),
-    then it is not saved again.
-    The result of each download attempt is saved to a webstore database, so the
+    The result of each download attempt is saved to the CKAN task_status table, so the
     information can be used later for QA analysis.
 
     Usage:
@@ -19,7 +22,7 @@ class Archiver(Command):
            - Archive all resources or just those belonging to a specific package 
              if a package id is provided
 
-        paster archiver clean        
+        paster archiver clean
             - Remove all archived resources
     """    
     summary = __doc__.split('\n')[0]
@@ -27,17 +30,6 @@ class Archiver(Command):
     min_args = 0
     max_args = 2 
     pkg_names = []
-
-    parser = Command.standard_parser(verbose=True)
-    existing_dests = [o.dest for o in parser.option_list]
-    if not 'limit' in existing_dests:
-        parser.add_option('-l', '--limit',
-            action='store',
-            dest='limit',
-            default=False,
-            help="""Limit the process to a number of packages.
-                    (Ignored if a package id is provided as an argument)"""
-        )
 
     def command(self):
         """
@@ -48,10 +40,34 @@ class Archiver(Command):
             return
 
         cmd = self.args[0]
+        self._load_config()
+        user = get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
+        context = json.dumps({
+            'site_url': config['ckan.site_url'],
+            'apikey': user.get('apikey')
+        })
+        api_url = urlparse.urljoin(config['ckan.site_url'], 'api/action')
 
         if cmd == 'update':
-            tasks.update.delay(unicode(self.args[1]) if len(self.args) > 1 else None)
+            if len(self.args) > 1:
+                data = json.dumps({'id': unicode(self.args[1])})
+                response = requests.post(api_url + '/package_show', data)
+                packages =  [json.loads(response.content).get('result')]
+            else:
+                response = requests.post(api_url + '/current_package_list_with_resources', "{}")
+                packages = json.loads(response.content).get('result')
+
+            logger.info("Number of datasets to archive: %d" % len(packages))
+
+            for package in packages:
+                logger.info("Archiving dataset: %s (%d resources)" % (package.get('name'), len(package.get('resources', []))))
+                for resource in package.get('resources', []):
+                    data = json.dumps(resource, {'model': model})
+                    tasks.update.delay(context, data) 
+
         elif cmd == 'clean':
             tasks.clean.delay()
+
         else:
             logger.error('Command %s not recognized' % (cmd,))
+
