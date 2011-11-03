bc869de2546f11bf0578e549760c6c5fdd59bba3
John Glover <glover.john@gmail.com>
2011-11-03 13:52:20 +0000

    add dataset_score function and code skeleton for resource_score function

--- ckanext/qa/tasks.py
+++ ckanext/qa/tasks.py
@@ -1,5 +1,5 @@
 """
-Score packages on Sir Tim Bernes-Lee's five stars of openness based on mime-type.
+Score datasets on Sir Tim Bernes-Lee's five stars of openness based on mime-type.
 """
 import datetime
 import mimetypes
@@ -65,12 +65,15 @@ def _update_task_status(context, data):
         raise CkanError('ckan failed to update task_status, status_code (%s), error %s' 
                         % (res.status_code, res.content))
 
+
 @task(name = "qa.update")
 def update(context, data):
+    logger = update.get_logger()
+
     try:
         data = json.loads(data)
         context = json.loads(context)
-        result = dataset_score(context, data)
+        result = dataset_score(context, data, logger)
         return json.dumps(result)
     except Exception, e:
         _update_task_status(context, {
@@ -84,105 +87,115 @@ def update(context, data):
         raise
 
 
-def dataset_score(package, results_file):
+def resource_score(context, data, logger):
     """
+    Score resources on Sir Tim Bernes-Lee's five stars of openness based on mime-type.
+
+    returns a dict with keys:
+
+        'resource_id': id (int) 
+        'openness_score': score (int)
+        'openness_score_reason': the reason for the score (string)
+        'openness_score_failure_count': the number of consecutive times this resource has returned a score of 0
+
+    Raises the following exceptions:
+
     """
-    package_extras = package.get('extras', [])
-    package_openness_score = '0'
-    return "{}"
+    id = data['id']
+    score = 0
+    score_reason = ""
+    score_failure_count = 0
 
-    for resource in package.get('resources'):
-        archive_result = get_resource_result(results_file, resource['id'])
+    # archive_result = get_resource_result(results_file, resource['id'])
 
-        openness_score = u'0'
-        openness_score_failure_count = int(
-            resource.get('openness_score_failure_count', 0)
-        )
+    # if not archive_result:
+    #     # set a default message if no archive result for this resource
+    #     reason = u"URL unobtainable"
+    # else:
+    #     reason = archive_result['message']
+    #     cl = archive_result['content_length']
+    #     ct = archive_result['content_type']
 
-        if not archive_result:
-            # set a default message if no archive result for this resource
-            reason = u"URL unobtainable"
-        else:
-            reason = archive_result['message']
-            cl = archive_result['content_length']
-            ct = archive_result['content_type']
+    #     if archive_result['success'] == 'True':
+    #         # also get format from resource and by guessing from file extension
+    #         format = resource.get('format', '').lower()
+    #         file_type = mimetypes.guess_type(resource.get('url'))[0] 
 
-            if archive_result['success'] == 'True':
-                # also get format from resource and by guessing from file extension
-                format = resource.get('format', '').lower()
-                file_type = mimetypes.guess_type(resource.get('url'))[0] 
+    #         # file type takes priority for scoring
+    #         if file_type:
+    #             openness_score = MIME_TYPE_SCORE.get(file_type, '-1')
+    #         elif ct:
+    #             openness_score = MIME_TYPE_SCORE.get(ct, '-1')
+    #         elif format:
+    #             openness_score = MIME_TYPE_SCORE.get(format, '-1')
+            
+    #         reason = OPENNESS_SCORE_REASON[openness_score]
 
-                # file type takes priority for scoring
-                if file_type:
-                    openness_score = MIME_TYPE_SCORE.get(file_type, '-1')
-                elif ct:
-                    openness_score = MIME_TYPE_SCORE.get(ct, '-1')
-                elif format:
-                    openness_score = MIME_TYPE_SCORE.get(format, '-1')
-                
-                reason = OPENNESS_SCORE_REASON[openness_score]
+    #         # check for mismatches between content-type, file_type and format
+    #         # ideally they should all agree
+    #         if not ct:
+    #             # TODO: use the todo extension to flag this issue
+    #             pass
+    #         else:
+    #             allowed_formats = [ct.lower().split('/')[-1], ct.lower().split('/')]
+    #             allowed_formats.append(ct.lower())
+    #             if format not in allowed_formats:
+    #                 # TODO: use the todo extension to flag this issue
+    #                 pass
+    #             if file_type != ct:
+    #                 # TODO: use the todo extension to flag this issue
+    #                 pass
 
-                # check for mismatches between content-type, file_type and format
-                # ideally they should all agree
-                if not ct:
-                    # TODO: use the todo extension to flag this issue
-                    pass
-                else:
-                    allowed_formats = [ct.lower().split('/')[-1], ct.lower().split('/')]
-                    allowed_formats.append(ct.lower())
-                    if format not in allowed_formats:
-                        # TODO: use the todo extension to flag this issue
-                        pass
-                    if file_type != ct:
-                        # TODO: use the todo extension to flag this issue
-                        pass
+    # # Set the failure count
+    # if openness_score == '0':
+    #     openness_score_failure_count += 1
+    # # update package openness score
+    # if openness_score > package_openness_score:
+    #     package_openness_score = openness_score
 
-        # Set the failure count
-        if openness_score == '0':
-            openness_score_failure_count += 1
-        # update package openness score
-        if openness_score > package_openness_score:
-            package_openness_score = openness_score
+    # # update the resource
+    # context = {
+    #     'id': resource['id'], 'model': model, 'session': model.Session, 
+    #     'user': MAINTENANCE_AUTHOR, 'extras_as_string': True
+    # }
+    # resource[u'openness_score'] = openness_score
+    # resource[u'openness_score_reason'] = reason
+    # resource[u'openness_score_failure_count'] = unicode(openness_score_failure_count)
+    # update.resource_update(context, resource)
+    # log.info('Score for resource: %s (%s)' % (openness_score, reason))
 
-        # update the resource
-        context = {
-            'id': resource['id'], 'model': model, 'session': model.Session, 
-            'user': MAINTENANCE_AUTHOR, 'extras_as_string': True
-        }
-        resource[u'openness_score'] = openness_score
-        resource[u'openness_score_reason'] = reason
-        resource[u'openness_score_failure_count'] = unicode(openness_score_failure_count)
-        update.resource_update(context, resource)
-        log.info('Score for resource: %s (%s)' % (openness_score, reason))
-
-    # package openness score
-    if not 'openness_score' in [e['key'] for e in package_extras]:
-        package_extras.append({
-            'key': u'openness_score',
-            'value': package_openness_score
-        })
-    else:
-        for e in package_extras:
-            if e['key'] == 'openness_score':
-                e['value'] = package_openness_score
-
-    # package openness score last checked
-    if not 'openness_score_last_checked' in [e['key'] for e in package_extras]:
-        package_extras.append({
-            'key': u'openness_score_last_checked',
-            'value': datetime.datetime.now().isoformat()
-        })
-    else:
-        for e in package_extras:
-            if e['key'] == 'openness_score_last_checked':
-                e['value'] = datetime.datetime.now().isoformat()
-    
-    context = {
-        'id': package['id'], 'model': model, 'session': model.Session, 
-        'user': MAINTENANCE_AUTHOR, 'extras_as_string': True
+    return {
+        'resource_id': id,
+        'openness_score': score,
+        'openness_score_reason': score_reason,
+        'openness_score_failure_count': score_failure_count
     }
-    package['extras'] = package_extras
-    update.package_update(context, package)
-    log.info('Finished QA analysis of package: %s (score = %s)' 
-        % (package['name'], package_openness_score))
+
+
+def dataset_score(context, data, logger):
+    """
+    Score datasets on Sir Tim Bernes-Lee's five stars of openness based on mime-type.
+    
+    Returns a dict with keys:
+
+        'openness_score': score for dataset (int)
+        'resource_scores': resource score dict for each resource in dataset (list)
+
+    Raises the following exceptions:
+
+    """
+    score = 0
+    resource_scores = []
+
+    for resource in data.get('resources'):
+        r = resource_score(context, resource, logger)
+        if r['openness_score'] > score:
+            score = r['openness_score']
+        resource_scores.append(r)
+
+    logger.info('Finished QA analysis of package: %s (score = %s)' 
+        % (data.get('name'), score)
+    )
+
+    return {'openness_score': score, 'resource_scores': resource_scores}
 
