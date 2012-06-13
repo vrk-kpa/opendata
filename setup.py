b2026158e0aec2c79bba9e61ff02c14d42166b20
John Glover <j@johnglover.net>
2012-06-13 15:51:05 +0100

    Change celery and kombu requirements to match ckanext-datastorer

--- setup.py
+++ setup.py
@@ -17,8 +17,8 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-        'celery==2.4.5',
-        'kombu==1.5.1',
+        'celery==2.4.2',
+        'kombu==2.1.3',
         'kombu-sqlalchemy==1.1.0',
         'SQLAlchemy>=0.6.6',
         'requests==0.6.4',
