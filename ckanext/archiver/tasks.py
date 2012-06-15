65ece35903d554a1f642aa697879e8824868e0b5
David Read <david.read@hackneyworkshop.com>
2012-06-15 18:39:09 +0100

    File permissions were 0600 because it was a tempfile, so made more liberal for DGU.

--- ckanext/archiver/tasks.py
+++ ckanext/archiver/tasks.py
@@ -320,7 +320,8 @@ def archive_resource(context, resource, logger, result=None, url_timeout = 30):
             file_name = "resource"
         saved_file = os.path.join(dir, file_name)
         shutil.move(result['saved_file'], saved_file)
-
+        os.chmod(saved_file, 0644) # allow other users to read it
+            
         # update the resource object: set cache_url and cache_last_updated
         if context.get('cache_url_root'):
             cache_url = urlparse.urljoin(
