22988a5eafa77c27e4091d6cf0dc93f3d096f072
David Read <david.read@hackneyworkshop.com>
2012-11-05 16:10:34 +0000

    Forgot logic file.

---
+++ ckanext/qa/logic.py
@@ -0,0 +1,27 @@
+import logging
+
+from ckan.plugins import PluginImplementations
+from ckan.model import Package
+from ckan.logic import NotFound, check_access, get_action
+from ckan.authz import Authorizer
+from ckan.lib.search import index_for
+
+log = logging.getLogger(__name__)
+
+def search_index_update(context, data_dict):
+    model = context['model']
+    session = context['session']
+    user = context.get('user')
+
+    if not Authorizer().is_sysadmin(user):
+        return {'success': False, 'msg': _('User %s not authorized to update harvest sources') % str(user)}
+    #check_access('search_index_update', context, data_dict)
+
+    pkg_dict = get_action('package_show')(
+        {'model': model, 'ignore_auth': True, 'validate': False},
+        data_dict)
+    
+    indexer = index_for('package')
+    indexer.update_dict(pkg_dict)
+    
+    log.info('Search index updated for: %s', pkg_dict['name'])
