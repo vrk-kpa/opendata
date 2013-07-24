df089392ac4407aa5b48820d94d82a9b4766738c
David Read <david.read@hackneyworkshop.com>
2013-07-24 15:34:26 +0100

    [#1038] Move helpers to logic layer. Add group_tree_section on organization/about/x page.

--- ckanext/hierarchy/plugin.py
+++ ckanext/hierarchy/plugin.py
@@ -1,5 +1,6 @@
 import ckan.plugins as p
 import ckanext.hierarchy.lib.helpers as helpers
+import ckanext.hierarchy.logic.action as action
 
 # This plugin is designed to work only these versions of CKAN
 p.toolkit.check_ckan_version(min_version='2.0')
@@ -9,7 +10,7 @@ class OrganizationHierarchy(p.SingletonPlugin):
     p.implements(p.IConfigurer, inherit=True)
     p.implements(p.IRoutes, inherit=True)
     p.implements(p.IGroupForm, inherit=True)
-    p.implements(p.ITemplateHelpers, inherit=True)
+    p.implements(p.IActions, inherit=True)
 
     def update_config(self, config):
         config['ckan.organization-hierarchy.enabled'] = True
@@ -20,9 +21,10 @@ class OrganizationHierarchy(p.SingletonPlugin):
     def before_map(self, map):
         # override the organization index route to use the group controller
         # instead of the organization controller, so that we can use a
-        # custom index_template that extends the existing one (so can't
+        # custom template that extends the existing one (so can't
         # override the template.)
         map.connect('organizations_index', '/organization', controller='group', action='index')
+        map.connect('organization_about', '/organization/about/{id}', controller='group', action='about', ckan_icon='info-sign')
         return map
 
     def index_template(self):
@@ -33,10 +35,11 @@ class OrganizationHierarchy(p.SingletonPlugin):
 
     def setup_template_variables(self, context, data_dict):
         p.toolkit.c.tree = 'ff'
-    
+
     def group_types(self):
         return ('organization',)
 
-    def get_helpers(self):
-        return {'group_tree': helpers.group_tree,
-                'group_tree_branch': helpers.group_tree_branch}
+    def get_actions(self):
+        return {'group_tree': action.group_tree,
+                'group_tree_section': action.group_tree_section,
+                }
