0dc98f30c7e80c3e1dbf4929c457564a0e2d3d24
David Read <david.read@hackneyworkshop.com>
2013-08-15 15:37:47 +0100

    [#1038] Simplify how the core CKAN templates are amended by this extension - use ckan_extends.

--- ckanext/hierarchy/plugin.py
+++ ckanext/hierarchy/plugin.py
@@ -8,9 +8,10 @@ p.toolkit.check_ckan_version(min_version='2.0')
 class OrganizationHierarchy(p.SingletonPlugin):
 
     p.implements(p.IConfigurer, inherit=True)
-    p.implements(p.IRoutes, inherit=True)
     p.implements(p.IGroupForm, inherit=True)
     p.implements(p.IActions, inherit=True)
+
+    # IConfigurer
 
     def update_config(self, config):
         config['ckan.organization-hierarchy.enabled'] = True
@@ -18,26 +19,12 @@ class OrganizationHierarchy(p.SingletonPlugin):
         p.toolkit.add_template_directory(config, 'public')
         p.toolkit.add_resource('public/scripts/vendor/jstree', 'jstree')
 
-    def before_map(self, map):
-        # override the organization index route to use the group controller
-        # instead of the organization controller, so that we can use a
-        # custom template that extends the existing one (so can't
-        # override the template.)
-        map.connect('organizations_index', '/organization', controller='group', action='index')
-        map.connect('organization_about', '/organization/about/{id}', controller='group', action='about', ckan_icon='info-sign')
-        return map
-
-    def index_template(self):
-        return 'organization/hierarchy_index.html'
-
-    def about_template(self):
-        return 'organization/hierarchy_about.html'
-
-    def setup_template_variables(self, context, data_dict):
-        p.toolkit.c.tree = 'ff'
+    # IGroupForm
 
     def group_types(self):
         return ('organization',)
+
+    # IActions
 
     def get_actions(self):
         return {'group_tree': action.group_tree,
