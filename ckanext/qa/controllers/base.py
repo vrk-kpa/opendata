f7c62cb507a23a2b333faf904bfee819116d639b
Ross Jones <ross@servercode.co.uk>
2013-10-28 15:35:50 +0000

    Remove breaking change

--- ckanext/qa/controllers/base.py
+++ ckanext/qa/controllers/base.py
@@ -2,6 +2,4 @@ from ckan.lib.base import BaseController
 from pylons import config
 
 class QAController(BaseController):
-
-    def __init__(self, *args, **kwargs):
-        super(QAController, self).__init(*args, **kwargs)
+    pass
