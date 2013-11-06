345773b4ab1025ce64318014ebdbe2ec52998482
David Read <david.read@hackneyworkshop.com>
2013-11-06 14:25:13 +0000

    Drop requirement for urllib3 - it is part of requests.

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setup(
         'SQLAlchemy>=0.6.6',
         'requests==1.1.0',
         'messytables>=0.1.4',
-	'urllib3==1.7',
         'flask==0.8'  # flask needed for tests
     ],
     author='Open Knowledge Foundation',
