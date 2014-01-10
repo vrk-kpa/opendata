12e31f09d812965739999572541dac4381df4481
Ilja <ilja@spam.fake>
2014-01-10 14:36:21 +0200

    YH-115: Disable accelerate in CI

--- scripts/travis-package.sh
+++ scripts/travis-package.sh
@@ -18,7 +18,7 @@ echo "  StrictHostKeyChecking no" >> ~/.ssh/config
 echo "[webservers]" > inventory-alpha
 echo "$SSHHOST" >> inventory-alpha
 echo $PASS | gpg --batch --passphrase-fd 0 --output=vars/private/variables.yml vars/private/variables-alpha.yml.gpg > /dev/null 2>&1
-sshpass -e ansible-playbook --inventory-file=inventory-alpha site.yml --user=deploy --ask-pass > ansible.output 2>&1
+sshpass -e ansible-playbook --inventory-file=inventory-alpha site.yml --user=deploy --ask-pass --extra-vars='accelerate=false' > ansible.output 2>&1
 sshpass -e scp ansible.output "$USERNAME@$SSHHOST:/home/$USERNAME/ansible_$DATE.output" > /dev/null 2>&1
 rm ansible.output
 rm vars/private/variables.yml
