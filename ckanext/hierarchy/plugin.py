9d43e0491090831a0387dcf79844cba2eb7fd7bd
David Read <david.read@hackneyworkshop.com>
2014-01-13 16:55:22 +0000

    Simplify the form.

--- ckanext/hierarchy/plugin.py
+++ ckanext/hierarchy/plugin.py
@@ -1,7 +1,6 @@
 import ckan.plugins as p
 import ckanext.hierarchy.logic.action as action
 from ckan.lib.plugins import DefaultOrganizationForm
-from ckan.logic.validators import no_loops_in_hierarchy
 
 # This plugin is designed to work only these versions of CKAN
 p.toolkit.check_ckan_version(min_version='2.0')
@@ -30,7 +29,7 @@ class HierarchyDisplay(p.SingletonPlugin):
 class HierarchyForm(p.SingletonPlugin, DefaultOrganizationForm):
 
     p.implements(p.IGroupForm, inherit=True)
-        
+
     # IGroupForm
 
     def group_types(self):
@@ -47,26 +46,4 @@ class HierarchyForm(p.SingletonPlugin, DefaultOrganizationForm):
         else:
             c.allowable_parent_groups = model.Group.all(
                                                 group_type='organization')
-
-    def form_to_db_schema_options(self, options):
-        schema = DefaultGroupForm.form_to_db_schema_options(self, options)
-        # simple 'parent' field on the form needs converting to 'groups'
-        schema['parent'] = [self._convert_parent_to_group]
-        return schema
-
-    @staticmethod
-    def _convert_parent_to_group(key, data, errors, context):
-        if ('parent',) in data:
-            parent = data.get(('parent',))
-            if ('groups',) not in data:
-                data[('groups',)] = []
-            data[('groups',)].append({'name': parent,
-                                      'capacity': 'parent'})
-            # need to check for loops here since 'groups' is already validated
-            # at this point. Keys are tulples now, but the validator expects
-            # strings.
-            data_ = {'groups': data.get(('groups',))}
-            if ('id',) in data:
-                data_['id'] = data.get(('id',)) 
-            no_loops_in_hierarchy(None, data_, None, context)
 
