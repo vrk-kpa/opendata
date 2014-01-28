602cc37d464690a329bdf92e27593d580a3df804
Ilja <ilja@spam.fake>
2014-01-28 11:44:25 +0200

    Use submodules for testing

--- scripts/travis-test.sh
+++ scripts/travis-test.sh
@@ -1,7 +1,5 @@
 #! /bin/sh -e
 
-PLUGINS="ckanext-ytp-groups ckanext-ytp-theme ckanext-ytp-drupal"
-
 SOURCE_DIRECTORY=`pwd`
 
 . /usr/lib/ckan/default/bin/activate
@@ -10,8 +8,7 @@ EXIT_STATUS=0
 
 echo "## nosetests ##"
 
-cd /usr/lib/ckan/default/src/
-for plugin in $PLUGINS; do
+for plugin in modules/*; do
     if [ -f $plugin/test.ini ]; then
         echo "Running nosetest for $plugin"
         cd $plugin
@@ -20,13 +17,12 @@ for plugin in $PLUGINS; do
         if [ "$NOSE_EXIT" != "0" ]; then
         	EXIT_STATUS=$NOSE_EXIT
         fi
-        cd /usr/lib/ckan/default/src/
+        cd $SOURCE_DIRECTORY
     fi
 done
 
-cd /usr/lib/ckan/default/src/
 echo "## flake8 ##"
-flake8 --max-line-length=120 $PLUGINS
+flake8 --max-line-length=120 modules/*
 FLAKE8_EXIT=$?
 
 if [ "$FLAKE8_EXIT" != "0" ]; then
