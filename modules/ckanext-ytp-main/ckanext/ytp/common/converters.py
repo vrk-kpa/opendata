033d01d5632790342f27d2c47f28e2005265443e
Ilja <ilja@spam.fake>
2014-04-14 16:55:53 +0300

    Removed debug code

--- modules/ckanext-ytp-main/ckanext/ytp/common/converters.py
+++ modules/ckanext-ytp-main/ckanext/ytp/common/converters.py
@@ -2,18 +2,12 @@ import json
 
 
 def to_list_json(value, context):
-    with open('/tmp/debug', 'a') as fp:
-        fp.write("to %s\n" % value)
-        fp.flush()
     if isinstance(value, basestring):
         value = [value]
     return json.dumps([item for item in value if item])
 
 
 def from_json_list(value, context):
-    with open('/tmp/debug', 'a') as fp:
-        fp.write("to %s\n" % value)
-        fp.flush()
     try:
         parsed_value = json.loads(value)
         if not isinstance(parsed_value, list):
