9e1b115a243782f3f8169a79bf315fa5da0ebac7
Ilja <ilja@spam.fake>
2014-04-16 14:56:43 +0300

    YH-284: Ability to remove multiple field values

--- modules/ckanext-ytp-main/ckanext/ytp/common/public/javascript/ytp_form.js
+++ modules/ckanext-ytp-main/ckanext/ytp/common/public/javascript/ytp_form.js
@@ -2,11 +2,19 @@
 $(document).ready(function() {
     $('.ytp-list').each(function(){
         /* Create add link for all ytp-list class elements. Add link clones the input. */
-        var addLink = $('<a href="#" class="ytp-add-input" style="margin-left: 3px;"><i class="icon-plus-sign-alt icon-large"></i></a>');
+        var addLink = $('<a href="javascript:void(0);" class="ytp-add-input" style="margin-left: 3px;"><i class="icon-plus-sign-alt icon-large"></i></a>');
         var input = $(this);
         var index = 0;
         addLink.click(function() {
-            addLink.before(input.clone().val("").attr('id', input.attr('id') + "-clone-" + (++index)));
+            var cloned = input.clone().val("").attr('id', input.attr('id') + "-clone-" + (++index));
+            addLink.after(cloned);
+            var removeLink = $('<a href="javascript:void(0);" class="ytp-add-input" style="margin-left: 3px;"><i class="icon-minus-sign-alt icon-large"></i></a>');
+            cloned.after(removeLink);
+            removeLink.click(function() {
+                cloned.val("").remove();
+                removeLink.remove();
+                return false;
+            });
             return false;
         });
         input.after(addLink);
