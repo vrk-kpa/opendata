60444a34663d06b76fa7a6759498511eec90df77
salum-ar <salum.abdul-rahman@gofore.com>
2014-04-25 16:24:10 +0300

    yh-261 fixed empty fullname

--- modules/ytp-assets-common/resources/templates/body-navigation.php
+++ modules/ytp-assets-common/resources/templates/body-navigation.php
@@ -46,7 +46,12 @@
                 <a href="/<?php echo $language->language; ?>/user/login" class="login"><?php echo t("Log in"); ?> &gt;</a></li>
                 <?php } else { ?>
                 <a href="/data/<?php echo $language->language; ?>/user/<?php global $user; print_r($user->name);?>">
-                	<?php global $user; $temp=  user_load($user->uid); $fullname=$temp->field_fullname['und'][0]['value'];
+                	<?php global $user; $temp=  user_load($user->uid);
+					if (isset($field_fullname['und'])) {
+						if (isset($field_fullname['und'][0]) ){
+							if ( isset($field_fullname['und'][0]['value'])){
+					 			$fullname=$temp->field_fullname['und'][0]['value'];
+					}}}
                 	if (isset($fullname)) { print_r($fullname);}else{ print_r($user->name);} ?></a> 
                 	| <a href="/<?php echo $language->language; ?>/user/logout" class="login"><?php echo t("Log out"); ?> &gt;</a></li>
                 <?php } ?>
