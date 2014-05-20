cc6c45acbb2712abe2d81d4d5b11a90cb6719ddb
salum-ar <salum.abdul-rahman@gofore.com>
2014-05-20 15:34:07 +0300

    yh-327 flake8 fix

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/plugin.py
+++ modules/ckanext-ytp-main/ckanext/ytp/dataset/plugin.py
@@ -152,14 +152,12 @@ class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         schema.update({'valid_from': [ignore_missing, date_validator, convert_to_extras]})
         schema.update({'valid_till': [ignore_missing, date_validator, convert_to_extras]})
         schema.update({'temporal_granularity': [ignore_missing, unicode, convert_to_extras]})
-        # schema.update({'temporal_coverage_from': [ignore_missing, date_validator, convert_to_extras]})
-        # schema.update({'temporal_coverage_to': [ignore_missing, date_validator, convert_to_extras]})
         schema.update({'update_frequency': [ignore_missing, unicode, convert_to_extras]})
         schema.update({'content_type': [ignore_missing, convert_to_tags_string('content_type')]})
 
         schema.update({'original_language': [ignore_missing, unicode, convert_to_extras]})
         schema.update({'translations': [ignore_missing, to_list_json, convert_to_extras]})
-        schema.update({'resources':{ 'temporal_coverage_from': [ignore_missing, date_validator],
+        schema.update({'resources': {'temporal_coverage_from': [ignore_missing, date_validator],
                        'temporal_coverage_to': [ignore_missing, date_validator]}})
         schema = add_languages_modify(schema, self._localized_fields)
 
@@ -188,8 +186,6 @@ class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
         schema.update({'valid_from': [convert_from_extras, ignore_missing]})
         schema.update({'valid_till': [convert_from_extras, ignore_missing]})
         schema.update({'temporal_granularity': [convert_from_extras, ignore_missing]})
-        # schema.update({'temporal_coverage_from': [convert_from_extras, ignore_missing]})
-        # schema.update({'temporal_coverage_to': [convert_from_extras, ignore_missing]})
         schema.update({'update_frequency': [convert_from_extras, ignore_missing]})
         schema.update({'content_type': [toolkit.get_converter('convert_from_tags')('content_type'), string_join, ignore_missing]})
 
