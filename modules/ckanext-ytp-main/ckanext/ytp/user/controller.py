eb897d845bea0f3d876b33f1ad753f0780478740
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-06-06 13:24:35 +0300

    fix flake8

--- modules/ckanext-ytp-main/ckanext/ytp/user/controller.py
+++ modules/ckanext-ytp-main/ckanext/ytp/user/controller.py
@@ -1,74 +1,74 @@
 from pylons import config
 
 from ckan import model
-from ckan.common import request, c, response
+from ckan.common import request, c, _
 from ckan.logic import get_action, NotFound, NotAuthorized, check_access
 from ckan.lib import helpers as h
 from ckan.controllers.user import UserController
-from ckan.lib.base import redirect, abort, validate, render
+from ckan.lib.base import abort, validate, render
 import ckan.new_authz as new_authz
 
 
 class YtpUserController(UserController):
 
-  # Modify original CKAN Edit user controller
-  def edit(self, id=None, data=None, errors=None, error_summary=None):
-    context = {'save': 'save' in request.params,
-               'schema': self._edit_form_to_db_schema(),
-               'model': model, 'session': model.Session,
-               'user': c.user, 'auth_user_obj': c.userobj,
-               'keep_apikey': True, 'keep_email': True
-    }
-    if id is None:
-      if c.userobj:
-        id = c.userobj.id
-      else:
-        abort(400, _('No user specified'))
-    data_dict = {'id': id}
+    # Modify original CKAN Edit user controller
+    def edit(self, id=None, data=None, errors=None, error_summary=None):
+        context = {'save': 'save' in request.params,
+                   'schema': self._edit_form_to_db_schema(),
+                   'model': model, 'session': model.Session,
+                   'user': c.user, 'auth_user_obj': c.userobj,
+                   'keep_apikey': True, 'keep_email': True
+                   }
+        if id is None:
+            if c.userobj:
+                id = c.userobj.id
+            else:
+                abort(400, _('No user specified'))
+        data_dict = {'id': id}
 
-    try:
-      check_access('user_update', context, data_dict)
-    except NotAuthorized:
-      abort(401, _('Unauthorized to edit a user.'))
+        try:
+            check_access('user_update', context, data_dict)
+        except NotAuthorized:
+            abort(401, _('Unauthorized to edit a user.'))
 
-    if (context['save']) and not data:
-      return self._save_edit(id, context)
+        if (context['save']) and not data:
+            return self._save_edit(id, context)
 
-    try:
-      old_data = get_action('user_show')(context, data_dict)
+        try:
+            old_data = get_action('user_show')(context, data_dict)
 
-      schema = self._db_to_edit_form_schema()
-      if schema:
-        old_data, errors = validate(old_data, schema)
+            schema = self._db_to_edit_form_schema()
+            if schema:
+                old_data, errors = validate(old_data, schema)
 
-      c.display_name = old_data.get('display_name')
-      c.user_name = old_data.get('name')
+            c.display_name = old_data.get('display_name')
+            c.user_name = old_data.get('name')
 
-      data = data or old_data
+            data = data or old_data
 
-    except NotAuthorized:
-      abort(401, _('Unauthorized to edit user %s') % '')
-    except NotFound:
-      abort(404, _('User not found'))
+        except NotAuthorized:
+            abort(401, _('Unauthorized to edit user %s') % '')
+        except NotFound:
+            abort(404, _('User not found'))
 
-    user_obj = context.get('user_obj')
+        user_obj = context.get('user_obj')
 
-    if not (new_authz.is_sysadmin(c.user)
-            or c.user == user_obj.name):
-      abort(401, _('User %s not authorized to edit %s') %
-            (str(c.user), id))
+        if not (new_authz.is_sysadmin(c.user)
+                or c.user == user_obj.name):
+            abort(401, _('User %s not authorized to edit %s') %
+                  (str(c.user), id))
 
-    errors = errors or {}
-    vars = {'data': data, 'errors': errors, 'error_summary': error_summary}
+        errors = errors or {}
+        vars = {'data': data, 'errors': errors, 'error_summary': error_summary}
 
-    self._setup_template_variables({'model': model,
-                                    'session': model.Session,
-                                    'user': c.user or c.author},
-                                   data_dict)
+        self._setup_template_variables({'model': model,
+                                        'session': model.Session,
+                                        'user': c.user or c.author},
+                                       data_dict)
 
-    c.is_myself = True
-    c.show_email_notifications = h.asbool(
-      config.get('ckan.activity_streams_email_notifications'))
-    c.form = render(self.edit_user_form, extra_vars=vars)
+        c.is_myself = True
+        c.show_email_notifications = h.asbool(
+            config.get('ckan.activity_streams_email_notifications'))
+        c.form = render(self.edit_user_form, extra_vars=vars)
 
-    return render('user/edit.html')
+        return render('user/edit.html')
