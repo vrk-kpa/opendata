06387dfdf83f39ef81c60ec505a1b0c761de8eb3
Ville Seppanen <ville.seppanen@gofore.com>
2014-06-07 23:24:56 +0300

    YH-392: Added stack deletion on cleanup, cleaned up code

--- modules/ytp-tools/continuous-deployment/deploy.py
+++ modules/ytp-tools/continuous-deployment/deploy.py
@@ -1,20 +1,24 @@
 #!/usr/bin/env python
 
-import boto.ec2
 import boto.exception
 import boto.cloudformation
-import pprint
 import time
 import os
 import shutil
 import subprocess
 import json
+import logging
 
 import settings
 import secrets
 
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s')
+log = logging.getLogger(__name__)
+log.setLevel(logging.INFO)
+
 
 class ContinuousDeployer:
+    """Wrapper around Ansible, Git and AWS CloudFormation for automated deployments."""
 
     ec2 = None
     cloudform = None
@@ -22,9 +26,9 @@ class ContinuousDeployer:
     deploy_path = ""
     commit_details = None
 
-    # Constructor
     def __init__(self, project_prefix):
         self.deploy_id = "cd-" + project_prefix + "-" + str(int(time.time() * 1000))
+        log.debug("Deployment ID: " + deploy.deploy_id)
         self.deploy_path = settings.relative_cache_path + self.deploy_id
         self.ec2 = boto.ec2.connect_to_region(settings.region,
                                               aws_access_key_id=secrets.aws_access_key_id,
@@ -32,15 +36,15 @@ class ContinuousDeployer:
         self.cloudform = boto.cloudformation.connect_to_region(settings.region,
                                                                aws_access_key_id=secrets.aws_access_key_id,
                                                                aws_secret_access_key=secrets.aws_secret_access_key)
-
         try:
             os.makedirs(self.deploy_path)
         except OSError:
-            print "Failed to create cache directory for deployment"
+            log.error("Failed to create cache directory for deployment")
             raise
 
-    # Clone and extract sources and store last commit information
     def prepare_sources(self):
+        """Clone and extract sources and store last commit information."""
+
         with open(os.devnull, "w") as devnull:
             subprocess.call(["git", "clone", settings.git_url_ytp], cwd=self.deploy_path, stdout=devnull, stderr=devnull)
 
@@ -48,7 +52,7 @@ class ContinuousDeployer:
                 git_log_format = "--pretty=format:{\"CommitId\":\"%H\",\"CommitDetails\":\"%an %ad %f\"}"
                 self.commit_details = json.loads(subprocess.check_output(["git", "log", "-1", git_log_format], cwd=self.deploy_path+"/ytp"))
             except:
-                print "Failed to get commit details"
+                log.error("Failed to get commit details")
                 raise
 
             subprocess.call(["ssh-agent bash -c 'ssh-add " + settings.keyfilename + "; cd " +
@@ -60,15 +64,9 @@ class ContinuousDeployer:
                                                stdin=subprocess.PIPE, stdout=devnull, stderr=devnull)
             process_extract.communicate(secrets.passphrase + "\n")
 
-    # Lists instances
-    def list_instances(self):
-        for reservation in self.ec2.get_all_reservations():
-            for instance in reservation.instances:
-                pprint.pprint(instance)
-                print instance.tags
-
-    # Create an infrastructure stack based on a cloudformation template
     def create_infrastructure_stack(self, template_filename):
+        """Create an infrastructure stack based on a cloudformation template."""
+
         with open(settings.relative_template_path + template_filename, "r") as template_file:
             template = template_file.read()
 
@@ -76,8 +74,9 @@ class ContinuousDeployer:
 
         self.cloudform.create_stack(self.deploy_id, template_body=template, parameters=stack_parameters, tags=self.commit_details, timeout_in_minutes=10)
 
-    # Wait for cloudformation to create the stack. Crude waiting done by polling, could be replaced with SNS
     def wait_for_stack_creation(self):
+        """Wait for cloudformation to create the stack. Crude waiting done by polling, could be replaced with SNS."""
+
         slept = 0
         while slept < settings.cloudformation_create_timeout:
             try:
@@ -87,40 +86,35 @@ class ContinuousDeployer:
                    events[0].resource_type == "AWS::CloudFormation::Stack" and
                    events[0].logical_resource_id == self.deploy_id and
                    events[0].resource_status == "CREATE_COMPLETE"):
-                    print "Stack is now ready"
+                    log.debug("Stack is now ready")
                     return
             except boto.exception.BotoServerError:
-                print "Server error, maybe stack does not exist yet"
+                log.warning("Server error, maybe stack does not exist yet")
 
             slept += settings.cloudformation_create_pollrate
             time.sleep(settings.cloudformation_create_pollrate)
         return
 
-    # Removes all files created for this deployment
     def cleanup(self):
-        print "Removing all temporary files"
+        """Delete stack and clean up all local files created for this deployment."""
+
+        try:
+            self.cloudform.delete_stack(self.deploy_id)
+        except:
+            log.warning("Failed to delete stack, something might be left running in AWS!")
+        log.debug("Removing all temporary files")
         shutil.rmtree(self.deploy_path)
 
 
-# Main function
 if __name__ == "__main__":
 
     deploy = ContinuousDeployer(settings.project_prefix)
 
-    print "Deployment id", deploy.deploy_id
-
-    # deploy.list_instances()
-
     deploy.prepare_sources()
-
     deploy.create_infrastructure_stack(settings.cloudformation_templatefile)
-
     deploy.wait_for_stack_creation()
-
-    # time.sleep(10)
     deploy.cleanup()
 
-    # startup infrastructure, wait for infra to come up
     # deploy with ansible and dynamic inventory
     # wait for deployment
     # if deploy comes up, run basic tests
