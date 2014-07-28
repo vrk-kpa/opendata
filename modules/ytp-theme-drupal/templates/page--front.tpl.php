c9fc65c23f95c82955ecce904df436e6bfabe9cd
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-07-28 14:10:49 +0300

    unify and translate frontpage search datasets

--- modules/ytp-theme-drupal/templates/page--front.tpl.php
+++ modules/ytp-theme-drupal/templates/page--front.tpl.php
@@ -3,7 +3,7 @@
 
 $ytp_custom_top = '<div class="navbar navbar-search form-control" role="search">' .
                   '  <form class="navbar-form" action="/data/' . $language->language . '/dataset">' .
-                  '    <input class="search-term" type="text" name="q" placeholder="' . t("Search datasets") . '">' .
+                  '    <input class="search-term" type="text" name="q" placeholder="' . t("Search datasets...") . '">' .
                   '    <button type="submit" class="search-submit" value="' . t("Search") . '" ><i class="icon-search"></i><span>' . t("Search") .'</span>'  .
                   '    <input type="hidden" name="sort" value="score desc, metadata_modified desc" />' .
                   '  </form>' .
