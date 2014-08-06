79ac981e2af22eb1d6e3ae5453f6f0fa7c2376b6
jsalonen <jaakko.salonen@iki.fi>
2014-08-06 12:52:48 +0300

    Run static_css task before other asset compilation steps

--- modules/ytp-assets-common/gulpfile.js
+++ modules/ytp-assets-common/gulpfile.js
@@ -55,14 +55,14 @@ gulp.task('templates', function() {
 });
 
 
-gulp.task('static_css', function(){
+gulp.task('static_css', function() {
     return gulp.src(paths.src.static_pages + "/css/main.css" )
         .pipe(base64({maxImageSize:512*1024}))
         .pipe(concat('style.css'))
         .pipe(gulp.dest(paths.src.static_pages + "/css"));
 });
 
-gulp.task('static_pages', ['static_css'], function() {
+gulp.task('static_pages', function() {
   return gulp.src(paths.src.static_pages + "/*.html")
       .pipe(inlineCss())
       .pipe(gulp.dest(paths.dist + '/static'));
@@ -97,6 +97,7 @@ gulp.task('config', function(){
 
 gulp.task('default', function(callback) {
   runSequence('clean',
+              'static_css',
               ['bootstrap', 'vendor','config', 'less', 'templates', 'static_pages', 'images', 'fonts'],
               callback);
 });
