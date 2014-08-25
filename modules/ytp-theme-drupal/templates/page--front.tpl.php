b4602e46e22aa3db7e942a75d92164053bc1cb71
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-08-25 14:45:06 +0300

    IE9 bugfix

--- modules/ytp-theme-drupal/templates/page--front.tpl.php
+++ modules/ytp-theme-drupal/templates/page--front.tpl.php
@@ -4,7 +4,7 @@
 $ytp_custom_top = '<div class="navbar navbar-search form-control" role="search">' .
                   '  <form class="navbar-form" action="/data/' . $language->language . '/dataset">' .
                   '    <input class="search-term" type="text" name="q" placeholder="' . t("Search datasets...") . '">' .
-                  '    <button type="submit" class="search-submit" value="' . t("Search") . '" ><i class="icon-search"></i><span>' . t("Search") .'</span>'  .
+                  '    <button type="submit" class="search-submit" value="' . t("Search") . '" ><i class="icon-search"></i><span>' . t("Search") .'</span></button>'  .
                   '    <input type="hidden" name="sort" value="score desc, metadata_modified desc" />' .
                   '  </form>' .
                   '</div>';
