0ae4485e1fcfd4948408a7e8e9d0ed86e90c21d5
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-08-26 15:35:28 +0300

    YH-523: sort facet items by active, count and lastly display name

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/helpers.py
+++ modules/ckanext-ytp-main/ckanext/ytp/dataset/helpers.py
@@ -31,3 +31,8 @@ def get_remaining_facet_item_count(facet, limit=10):
     items = c.search_facets.get(facet)['items']
     return len(items) - 1 - limit
 
+def sort_facet_items_by_name(items):
+    sorted_items = []
+    sorted_items.extend(sorted([item for item in items if item['active'] == True] , key=lambda item: (-item['count'], item['display_name'])))
+    sorted_items.extend(sorted([item for item in items if item['active'] == False] , key=lambda item: (-item['count'], item['display_name'])))
+    return sorted_items
