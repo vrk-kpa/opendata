aab768b0f4c5134e4aced839efaf59699a07fa98
Ilja <ilja.pyykkonen@gofore.com>
2014-09-01 14:29:51 +0300

    Fixed file upload translation

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/translations.py
+++ modules/ckanext-ytp-main/ckanext/ytp/dataset/translations.py
@@ -2,6 +2,7 @@
 def _translations():
     """ Does nothing but hints message extractor to extract missing strings. """
     from ckan.common import _
+    # Dataset error labels (converted field names)
     _("Valid from")
     _("Valid till")
     _("Title en")
@@ -11,6 +12,9 @@ def _translations():
     _("Notes")
     _("Content type")
     _("Owner org")
+
+    # Missing from CKAN translation
+    _('File upload too large')
 
 
 def facet_translations():
