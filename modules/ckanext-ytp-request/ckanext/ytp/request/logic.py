b1927b5a72e3e4d47d93af4290456ab4955889b7
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-10-01 12:31:53 +0300

    YH-577: add logging to membership approvals

--- modules/ckanext-ytp-request/ckanext/ytp/request/logic.py
+++ modules/ckanext-ytp-request/ckanext/ytp/request/logic.py
@@ -258,10 +258,20 @@ def _process_request(context, member, action):
     member_user = model.Session.query(model.User).get(member.table_id)
 
     locale = member.extras.get('locale', None) or _get_default_locale()
+    _log_process(member_user, member.group.display_name, approve)
     _mail_process_status(locale, member_user, approve, member.group.display_name, member.capacity)
 
     return model_dictize.member_dictize(member, context)
 
+def _log_process(member_user, member_org, approve):
+    if approve:
+        log.info("Membership request of %s approved to %s by admin: %s" %
+                 (member_user.fullname if member_user.fullname else member_user.name, member_org,
+                  c.userobj.fullname if c.userobj.fullname else c.userobj.name) )
+    else:
+        log.info("Membership request of %s rejected to %s by admin: %s" %
+                 (member_user.fullname if member_user.fullname else member_user.name, member_org,
+                  c.userobj.fullname if c.userobj.fullname else c.userobj.name) )
 
 def member_request_membership_cancel(context, data_dict):
     ''' Cancel organization membership (not request). Member or organization_id must be provided.
