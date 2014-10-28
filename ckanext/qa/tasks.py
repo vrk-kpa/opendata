9bd113ef0f38d85424554fa6299254da24bd476a
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-10-28 15:43:07 +0200

    we don't care about open licence since we mostly harvest from elsewhere and we assume that the used license is open

--- ckanext/qa/tasks.py
+++ ckanext/qa/tasks.py
@@ -35,6 +35,7 @@ MIME_TYPE_SCORE = {
     'text/plain': 1,
     'text': 1,
     'txt': 1,
+    'text/html': 1,
     'application/vnd.ms-excel': 2,
     'application/vnd.ms-excel.sheet.binary.macroenabled.12': 2,
     'application/vnd.ms-excel.sheet.macroenabled.12': 2,
@@ -189,48 +190,44 @@ def resource_score(context, data):
     if json.loads(response.content)['success']:
         score_failure_count = int(json.loads(response.content)['result'].get('value', '0'))
 
-    # no score for resources that don't have an open license
-    if not data.get('is_open'):
-        score_reason = 'License not open'
-    else:
-        try:
-            headers = link_checker("{}", json.dumps(data))
-            ct = headers.get('content-type')
+    try:
+        headers = link_checker("{}", json.dumps(data))
+        ct = headers.get('content-type')
 
-            # ignore charset if exists (just take everything before the ';')
-            if ct and ';' in ct:
-                ct = ct.split(';')[0]
+        # ignore charset if exists (just take everything before the ';')
+        if ct and ';' in ct:
+            ct = ct.split(';')[0]
 
-            # also get format from resource and by guessing from file extension
-            format = data.get('format', '').lower()
-            file_type = mimetypes.guess_type(data['url'])[0]
+        # also get format from resource and by guessing from file extension
+        format = data.get('format', '').lower()
+        file_type = mimetypes.guess_type(data['url'])[0]
 
-            # file type takes priority for scoring
-            if file_type:
-                score = MIME_TYPE_SCORE.get(file_type, -1)
-            elif ct:
-                score = MIME_TYPE_SCORE.get(ct, -1)
-            elif format:
-                score = MIME_TYPE_SCORE.get(format, -1)
+        # file type takes priority for scoring
+        if file_type:
+            score = MIME_TYPE_SCORE.get(file_type, -1)
+        elif ct:
+            score = MIME_TYPE_SCORE.get(ct, -1)
+        elif format:
+            score = MIME_TYPE_SCORE.get(format, -1)
 
-            score_reason = OPENNESS_SCORE_REASON[score]
+        score_reason = OPENNESS_SCORE_REASON[score]
 
-            # negative scores are only useful for getting the reason message,
-            # set it back to 0 if it's still <0 at this point
-            if score < 0:
-                score = 0
+        # negative scores are only useful for getting the reason message,
+        # set it back to 0 if it's still <0 at this point
+        if score < 0:
+            score = 0
 
-            # check for mismatches between content-type, file_type and format
-            # ideally they should all agree
-            if not ct:
-                # TODO: use the todo extension to flag this issue
-                pass
+        # check for mismatches between content-type, file_type and format
+        # ideally they should all agree
+        if not ct:
+            # TODO: use the todo extension to flag this issue
+            pass
 
-        except LinkCheckerError, e:
-            score_reason = str(e)
-        except Exception, e:
-            log.error('Unexpected error while calculating openness score %s: %s', e.__class__.__name__,  unicode(e))
-            score_reason = "Unknown error: %s" % str(e)
+    except LinkCheckerError, e:
+        score_reason = str(e)
+    except Exception, e:
+        log.error('Unexpected error while calculating openness score %s: %s', e.__class__.__name__,  unicode(e))
+        score_reason = "Unknown error: %s" % str(e)
 
     if score == 0:
         score_failure_count += 1
