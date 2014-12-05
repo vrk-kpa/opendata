d70c4bfd6b42bc8a6c5418548291c0dc452b78e3
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-12-05 11:06:25 +0200

    YH-620: refactor organization list

--- modules/ckanext-ytp-main/ckanext/ytp/common/public/javascript/ytp_form.js
+++ modules/ckanext-ytp-main/ckanext/ytp/common/public/javascript/ytp_form.js
@@ -259,7 +259,7 @@ $(document).ready(function(){
         $(this).datetimepicker({
             pickTime: false
         });
-    })
+    });
 
     $('[data-tree]').each(function(){
         var TreeConfig = JSON.parse($(this).attr('attrs'));
@@ -271,6 +271,48 @@ $(document).ready(function(){
         else{
             $(this).jstree(TreeConfig);
         }
+    });
+
+    $('[data-organization-tree] .js-expand').bind('click', function(){
+        $(this).siblings('.js-collapse, .js-collapsed').show();
+        $(this).hide();
+    });
+
+    $('[data-organization-tree] .js-collapse').bind('click', function(){
+        $(this).hide().siblings('.js-collapsed').hide();
+        $(this).siblings('.js-expand').show();
+    });
+
+    $('[data-organization-filter]').bind('keyup cut paste', function(){
+        var search_str = $(this).val().toLowerCase();
+
+        if (search_str.length != 0){
+            $('.js-collapse, .js-expand').hide();
+            $('.js-collapsed').show();
+            var organizations = $('.organization-row');
+            organizations.hide();
+            organizations.filter(function(index, element){
+
+                return element.innerText.toLowerCase().indexOf(search_str) >= 0
+
+            }).each(function(index, element){
+                var text = element.innerText;
+                var str_index = text.toLowerCase().indexOf(search_str);
+                if ( str_index >= 0){
+                    $(this).find('a').html(text.substr(0, str_index) + '<span class="highlight">' + text.substr(str_index, search_str.length) + '</span>' + text.substr(str_index + search_str.length))[0];
+                }
+            }).show();
+
+        }
+        else{
+            $('span.highlight').contents().unwrap()
+            $('.organization-row').show();
+            $('.js-collapsed').hide();
+            $('.js-expand').show();
+            //$('.organization-tree').children('.organization').children('.organization-row').show()
+        }
+
+
     })
 });
 
