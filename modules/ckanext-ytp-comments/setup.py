bf041a25b16393d2b062d0319fd312d79909097f
Jari Voutilainen <jari.voutilainen@iki.fi>
2014-12-31 14:02:25 +0200

    create template

---
+++ modules/ckanext-ytp-comments/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.0'
+
+setup(
+    name='ckanext-ytp-comments',
+    version=version,
+    description="",
+    long_description='''
+    ''',
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='',
+    author='',
+    author_email='',
+    url='',
+    license='',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    namespace_packages=['ckanext', 'ckanext.ytp.comments'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        # -*- Extra requirements: -*-
+    ],
+    entry_points='''
+        [ckan.plugins]
+        # Add plugins here, e.g.
+        # myplugin=ckanext.ytp.comments.plugin:PluginClass
+    ''',
+)
