f6f21dd175a25dafa69ccbe33b14aee09b538f57
Jari Voutilainen <jari.voutilainen@iki.fi>
2015-02-25 14:00:36 +0200

    YH-712: support ie9 in related linking

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/public/javascript/related.js
+++ modules/ckanext-ytp-main/ckanext/ytp/dataset/public/javascript/related.js
@@ -1,11 +1,6 @@
 function SelectByName(event){
-    console.log(event)
     if (!event.added.created || event.added.created == false){
-        $('#field-url').val(window.location.origin + "/data/dataset/" + event.added.id)
+        $('#field-url').val(ckan.url('dataset/' + event.added.id));
         $('#field-title').val(event.added.text)
-
-    }
-    else{
-
     }
 }
