d26b752f7caa1bd739a488fed6c22d67905f8e92
Teemu Erkkola <teemu.erkkola@gofore.com>
2015-05-11 13:39:14 +0300

    YH-767: Show organization not found page if organization does not exist or if it is not active and user is not authorized

--- modules/ckanext-ytp-main/ckanext/ytp/organizations/plugin.py
+++ modules/ckanext-ytp-main/ckanext/ytp/organizations/plugin.py
@@ -311,6 +311,8 @@ class YtpOrganizationsPlugin(plugins.SingletonPlugin, DefaultOrganizationForm):
         with SubMapper(map, controller=organization_controller) as m:
             m.connect('organization_members', '/organization/members/{id}',
                       action='members', ckan_icon='group')
+            m.connect('organization_read', '/organization/{id}',
+                      action='read', ckan_icon='group')
             m.connect('/user_list', action='user_list', ckan_icon='user')
         return map
 
