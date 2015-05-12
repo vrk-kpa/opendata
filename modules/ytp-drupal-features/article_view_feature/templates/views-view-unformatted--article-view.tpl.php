3ef84c4c943d56205a061f9bb9d13260689a869d
Jari Voutilainen <jari.voutilainen@iki.fi>
2015-05-12 12:35:04 +0300

    fix indexing

--- modules/ytp-drupal-features/article_view_feature/templates/views-view-unformatted--article-view.tpl.php
+++ modules/ytp-drupal-features/article_view_feature/templates/views-view-unformatted--article-view.tpl.php
@@ -5,7 +5,7 @@
     <?php if (count($rows) >= 1): ?>
         <div class="row">
             <div class="col-lg-12 col-md-12 col-sm-12">
-                <div<?php if ($classes_array[$id]) { print ' class="' . $classes_array[$id] .'"'; } ?>>
+                <div<?php if ($classes_array[0]) { print ' class="' . $classes_array[0] .'"'; } ?>>
                     <?php print $rows[0]; ?>
                 </div>
             </div>
@@ -16,7 +16,7 @@
     <?php for($i = 1, $size = count($rows); $i < $size; ++$i) { ?>
 
         <div class="col-lg-6 col-md-6 col-sm-6">
-            <div<?php if ($classes_array[$i]) { print ' class="' . $classes_array[$i] .'"'; } ?>>
+            <div<?php if ($classes_array[0]) { print ' class="' . $classes_array[0] .'"'; } ?>>
                 <?php print $rows[$i]; ?>
             </div>
         </div>
