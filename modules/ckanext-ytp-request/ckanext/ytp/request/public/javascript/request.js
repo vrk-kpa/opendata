b0c117124b25c6fdbe8f658a03b5b62262617d82
Oscar Perez <oscar.perez@gofore.com>
2015-10-22 16:52:57 +0300

    Workaround to allow role change by admin

--- modules/ckanext-ytp-request/ckanext/ytp/request/public/javascript/request.js
+++ modules/ckanext-ytp-request/ckanext/ytp/request/public/javascript/request.js
@@ -1,4 +1,17 @@
 $(document).ready(function() {
+    $('#field-validroles').on('change', function(e) {
+      var selected_role = $("#field-validroles option:selected").text();
+      //Modify the URLs for accept and reject cases
+      var approve_url_element = $('#request_approve_url');
+      var reject_url_element = $('#request_reject_url');
+      var approve_url = approve_url_element.getAttribute('href').split('&', 2);
+      var reject_url = reject_url_element.getAttribute('href').split('&',2);
+      newapp_url = approve_url[0] + '&role=' + selected_role;
+      newrej_url = reject_url[0] +  '&role=' + selected_role;
+      approve_url_element.setAttribute('href', newapp_url);
+      reject_url.element.setAttribute('href', newrej_url);
+    });
+    
     $('#field-organizations').on('change', function(e) {
    	    //Setting the default selected roleto admin (in case editor was selected and now does not exist)
  		var roles = $('#role');
