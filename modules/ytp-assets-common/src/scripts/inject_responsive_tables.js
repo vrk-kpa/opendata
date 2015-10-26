2795e5537325b8909851c3169dc53d9ac9d20cbd
Teemu Erkkola <teemu.erkkola@gofore.com>
2015-10-26 13:28:53 +0200

    Fix error messages for injecting responsive tables where no tables exist

--- modules/ytp-assets-common/src/scripts/inject_responsive_tables.js
+++ modules/ytp-assets-common/src/scripts/inject_responsive_tables.js
@@ -2,15 +2,17 @@ var injectResponsiveTables = function (next) {
   document.onreadystatechange = function () {
     if (document.readyState == "interactive") {
       var article = document.getElementById("block-system-main");
-      var tables = article.getElementsByTagName("table");
-      for (var i = 0; i < tables.length; ++i) {
-        var wrapper = document.createElement("div");
-        if(tables[i].border === "0") {
-          tables[i].classList.add("table");
+      if(article !== null) {
+        var tables = article.getElementsByTagName("table");
+        for (var i = 0; i < tables.length; ++i) {
+          var wrapper = document.createElement("div");
+          if(tables[i].border === "0") {
+            tables[i].classList.add("table");
+          }
+          wrapper.classList.add("table-responsive");
+          tables[i].parentElement.insertBefore(wrapper, tables[i]);
+          wrapper.appendChild(tables[i]);
         }
-        wrapper.classList.add("table-responsive");
-        tables[i].parentElement.insertBefore(wrapper, tables[i]);
-        wrapper.appendChild(tables[i]);
       }
     }
     if(next) {
