ca91e229382a1525add972c79a253853eabb8c4b
David Read <david.read@hackneyworkshop.com>
2015-10-27 14:39:55 +0000

    Use test-core.ini as that is postgres.

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -27,7 +27,7 @@ cd ckan
 paster db init -c test-core.ini
 cd -
 
-echo "Installing ckanext-ckanext-archiver and its requirements..."
+echo "Installing ckanext-archiver and its requirements..."
 python setup.py develop
 pip install -r dev-requirements.txt
 
