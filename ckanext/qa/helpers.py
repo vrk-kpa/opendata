d81a8662f901f23e1fde079c286868a3c4683fb5
David Read <david.read@hackneyworkshop.com>
2015-11-20 16:52:14 +0000

    Hide the remarks

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -5,7 +5,7 @@ from ckan.plugins import toolkit as tk
 def qa_openness_stars_resource_html(resource):
     qa = resource.get('qa')
     if not qa:
-        return '<!-- No qa info for this resource -->'
+        return tk.literal('<!-- No qa info for this resource -->')
     # Take a copy of the qa dict, because weirdly the renderer appears to add
     # keys to it like _ and app_globals. This is bad because when it comes to
     # render the debug in the footer those extra keys take about 30s to render,
@@ -19,7 +19,7 @@ def qa_openness_stars_resource_html(resource):
 def qa_openness_stars_dataset_html(dataset):
     qa = dataset.get('qa')
     if not qa:
-        return '<!-- No qa info for this dataset -->'
+        return tk.literal('<!-- No qa info for this dataset -->')
     extra_vars = copy.deepcopy(qa)
     return tk.literal(
         tk.render('qa/openness_stars_brief.html',
