91c502beb68acff1ad5194a572d5b75f607b7d00
David Read <david.read@hackneyworkshop.com>
2015-11-20 00:21:22 +0000

    Fix bug that meant rendering debug info was v slow.

--- ckanext/qa/helpers.py
+++ ckanext/qa/helpers.py
@@ -1,3 +1,4 @@
+import copy
 from ckan.plugins import toolkit as tk
 
 
@@ -5,7 +6,11 @@ def qa_openness_stars_resource_html(resource):
     qa = resource.get('qa')
     if not qa:
         return '<!-- No qa info for this resource -->'
-    extra_vars = qa
+    # Take a copy of the qa dict, because weirdly the renderer appears to add
+    # keys to it like _ and app_globals. This is bad because when it comes to
+    # render the debug in the footer those extra keys take about 30s to render,
+    # for some reason.
+    extra_vars = copy.deepcopy(qa)
     return tk.literal(
         tk.render('qa/openness_stars.html',
                   extra_vars=extra_vars))
@@ -15,7 +20,7 @@ def qa_openness_stars_dataset_html(dataset):
     qa = dataset.get('qa')
     if not qa:
         return '<!-- No qa info for this dataset -->'
-    extra_vars = qa
+    extra_vars = copy.deepcopy(qa)
     return tk.literal(
         tk.render('qa/openness_stars_brief.html',
                   extra_vars=extra_vars))
