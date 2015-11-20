48ddaa09d08e5c617b2c250ae48757d923803926
David Read <david.read@hackneyworkshop.com>
2015-11-20 13:08:50 +0000

    Travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -7,7 +7,7 @@ echo "This is travis-build.bash..."
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
-sudo apt-get install libmagic
+sudo apt-get install libmagic1
 
 echo "Installing CKAN and its Python dependencies..."
 git clone https://github.com/datagovuk/ckan
