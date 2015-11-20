171697ff122ff1131996f7dca1ab4a00ed3f4847
David Read <david.read@hackneyworkshop.com>
2015-11-20 13:29:17 +0000

    Travis

--- bin/travis-build.bash
+++ bin/travis-build.bash
@@ -7,6 +7,9 @@ echo "This is travis-build.bash..."
 echo "Installing the packages that CKAN requires..."
 sudo apt-get update -qq
 sudo apt-get install postgresql-$PGVERSION solr-jetty libcommons-fileupload-java:amd64=1.2.2-1
+
+echo "Upgrading libmagic for ckanext-qa..."
+# appears to upgrade it from 5.09-2 to 5.09-2ubuntu0.6 which seems to help the tests
 sudo apt-get install libmagic1
 
 echo "Installing CKAN and its Python dependencies..."
