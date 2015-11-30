4f0e51feb9d1abbea5a9f276ef1d89b658facfcc
Jari Voutilainen <jari.voutilainen@iki.fi>
2015-11-30 13:13:13 +0200

    translations

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/plugin.py
+++ modules/ckanext-ytp-main/ckanext/ytp/dataset/plugin.py
@@ -521,9 +521,9 @@ class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
                user_name = self._get_user_by_id(user_id)
                accepted_harvesters = {'default', 'harvest'}
                if user_name in accepted_harvesters:
-                  pkg_dict['source'] = 'harvester' 
+                  pkg_dict['source'] = _('External')
                else:
-                  pkg_dict['source'] = 'manual'
+                  pkg_dict['source'] = _('Internal')
         return pkg_dict
 
     # IActions #
