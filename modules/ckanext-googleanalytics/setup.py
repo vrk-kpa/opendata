2822a0165ab3465c6b2388751e81f70f123aecea
Oscar Perez <oscar.perez@gofore.com>
2015-12-07 15:36:15 +0200

    Added init file for namespace

--- modules/ckanext-googleanalytics/setup.py
+++ modules/ckanext-googleanalytics/setup.py
@@ -19,7 +19,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     install_requires=[
-          # -*- Extra requirements: -*-
+       # Requirements defined in pip-requirements.txt
     ],
     entry_points='''
     [paste.paster_command]
