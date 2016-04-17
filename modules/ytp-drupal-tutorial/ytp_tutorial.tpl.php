fe17557e2a7a989100111d5f72a05f4eaff80435
Meepu <meeri.panula@gofore.com>
2016-04-17 20:55:08 +0300

    Style tutorial block

--- modules/ytp-drupal-tutorial/ytp_tutorial.tpl.php
+++ modules/ytp-drupal-tutorial/ytp_tutorial.tpl.php
@@ -1,10 +1,12 @@
 <div class="tutorial-box-content">
   <div class="media">
-    <div class="media-left">
+    <div class="media-icon">
       <img src="<?php print $image_url; ?>" class="media-object" alt=""/>
     </div>
+    <div class="media-heading">
+      <?php print render($heading); ?>
+    </div>
     <div class="media-body">
-      <h1 class="media-heading"><?php print $title; ?></h1>
       <?php print render($content); ?>
     </div>
   </div>
