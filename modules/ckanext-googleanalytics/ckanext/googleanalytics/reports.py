5a4c089c16d7ecd258aec0bba8b438c48e3cc435
Meepu <meeri.panula@gofore.com>
2016-08-08 17:51:26 +0300

    Fix merge conflict in reports

--- modules/ckanext-googleanalytics/ckanext/googleanalytics/reports.py
+++ modules/ckanext-googleanalytics/ckanext/googleanalytics/reports.py
@@ -54,7 +54,3 @@ googleanalytics_resource_report_info = {
     'generate': google_analytics_resource_report,
     'template': 'report/resource_analytics.html'
 }
-<<<<<<< HEAD
-=======
-
->>>>>>> YH-810
