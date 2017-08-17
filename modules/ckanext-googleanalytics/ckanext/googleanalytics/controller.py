742cdc69bfaa0c66b986f688339d7ca18cc86686
Jari Voutilainen <jari.voutilainen@iki.fi>
2017-08-17 09:13:18 +0300

    refactor ugly inheritance

--- modules/ckanext-googleanalytics/ckanext/googleanalytics/controller.py
+++ modules/ckanext-googleanalytics/ckanext/googleanalytics/controller.py
@@ -14,7 +14,7 @@ from paste.util.multidict import MultiDict
 
 from ckan.controllers.api import ApiController
 from ckan.controllers.package import PackageController
-from ckan.plugins import toolkit
+import ckan.plugins as p
 
 
 log = logging.getLogger('ckanext.googleanalytics')
@@ -107,61 +107,38 @@ class GAApiController(ApiController):
         self._post_analytics(c.user, register, "search", id)
 
         return ApiController.search(self, ver, register)
-        
 
+
+BaseClass = PackageController
 try:
-    if toolkit.plugin_loaded('cloudstorage'):
+    if p.plugin_loaded('cloudstorage'):
         from ckanext.cloudstorage.controller import StorageController
-        class GAResourceController(StorageController):
-            # intercept API calls to record via google analytics
-            def _post_analytics(
-                    self, user, request_obj_type, request_function, request_id):
-                if config.get('googleanalytics.id'):
-                    data_dict = {
-                        "v": 1,
-                        "tid": config.get('googleanalytics.id'),
-                        "cid": hashlib.md5(user).hexdigest(),
-                        # customer id should be obfuscated
-                        "t": "event",
-                        "dh": c.environ['HTTP_HOST'],
-                        "dp": c.environ['PATH_INFO'],
-                        "dr": c.environ.get('HTTP_REFERER', ''),
-                        "ec": "CKAN Resource Download Request",
-                        "ea": request_obj_type+request_function,
-                        "el": request_id,
-                    }
-                    plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
-
-        def resource_download(self, id, resource_id, filename=None):
-            self._post_analytics(c.user, "Resource", "Download", resource_id)
-            return StorageController.resource_download(self, id, resource_id,
-                                                       filename)
-    else:
-        raise ImportError
-
-except ImportError:
-    class GAResourceController(PackageController):
-        # intercept API calls to record via google analytics
-        def _post_analytics(
-                self, user, request_obj_type, request_function, request_id):
-            if config.get('googleanalytics.id'):
-                data_dict = {
-                    "v": 1,
-                    "tid": config.get('googleanalytics.id'),
-                    "cid": hashlib.md5(user).hexdigest(),
-                    # customer id should be obfuscated
-                    "t": "event",
-                    "dh": c.environ['HTTP_HOST'],
-                    "dp": c.environ['PATH_INFO'],
-                    "dr": c.environ.get('HTTP_REFERER', ''),
-                    "ec": "CKAN Resource Download Request",
-                    "ea": request_obj_type+request_function,
-                    "el": request_id,
-                }
-                plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
-
-        def resource_download(self, id, resource_id, filename=None):
-            self._post_analytics(c.user, "Resource", "Download", resource_id)
-            return PackageController.resource_download(self, id, resource_id,
-                                                       filename)
+        BaseClass = StorageController
+except:
     pass
+
+
+class GAResourceController(BaseClass):
+    # intercept API calls to record via google analytics
+    def _post_analytics(
+            self, user, request_obj_type, request_function, request_id):
+        if config.get('googleanalytics.id'):
+            data_dict = {
+                "v": 1,
+                "tid": config.get('googleanalytics.id'),
+                "cid": hashlib.md5(user).hexdigest(),
+                # customer id should be obfuscated
+                "t": "event",
+                "dh": c.environ['HTTP_HOST'],
+                "dp": c.environ['PATH_INFO'],
+                "dr": c.environ.get('HTTP_REFERER', ''),
+                "ec": "CKAN Resource Download Request",
+                "ea": request_obj_type+request_function,
+                "el": request_id,
+            }
+            plugin.GoogleAnalyticsPlugin.analytics_queue.put(data_dict)
+
+    def resource_download(self, id, resource_id, filename=None):
+        self._post_analytics(c.user, "Resource", "Download", resource_id)
+        return PackageController.resource_download(self, id, resource_id,
+                                                   filename)
