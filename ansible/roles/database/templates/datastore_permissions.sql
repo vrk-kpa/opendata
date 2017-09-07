244b9689e110b7459ba03d5a8f806cea68fa6a0f
Jari Voutilainen <jari.voutilainen@iki.fi>
2017-09-07 15:00:04 +0300

    add missing datastore permissions

--- ansible/roles/database/templates/datastore_permissions.sql
+++ ansible/roles/database/templates/datastore_permissions.sql
@@ -48,3 +48,25 @@ GRANT SELECT ON ALL TABLES IN SCHEMA public TO "{{ postgres.users.ckan_datastore
 -- grant access to new tables and views by default
 ALTER DEFAULT PRIVILEGES FOR USER "{{ postgres.users.ckan.username }}" IN SCHEMA public
    GRANT SELECT ON TABLES TO "{{ postgres.users.ckan_datastore.username }}";
+
+CREATE OR REPLACE VIEW "_table_metadata" AS
+    SELECT DISTINCT
+        substr(md5(dependee.relname || COALESCE(dependent.relname, '')), 0, 17) AS "_id",
+        dependee.relname AS name,
+        dependee.oid AS oid,
+        dependent.relname AS alias_of
+        -- dependent.oid AS oid
+    FROM
+        pg_class AS dependee
+        LEFT OUTER JOIN pg_rewrite AS r ON r.ev_class = dependee.oid
+        LEFT OUTER JOIN pg_depend AS d ON d.objid = r.oid
+        LEFT OUTER JOIN pg_class AS dependent ON d.refobjid = dependent.oid
+    WHERE
+        (dependee.oid != dependent.oid OR dependent.oid IS NULL) AND
+        (dependee.relname IN (SELECT tablename FROM pg_catalog.pg_tables)
+            OR dependee.relname IN (SELECT viewname FROM pg_catalog.pg_views)) AND
+        dependee.relnamespace = (SELECT oid FROM pg_namespace WHERE nspname='public')
+    ORDER BY dependee.oid DESC;
+ALTER VIEW "_table_metadata" OWNER TO "{{ postgres.users.ckan.username }}";
+GRANT SELECT ON "_table_metadata" TO "{{ postgres.users.ckan_datastore.username }}";
+
