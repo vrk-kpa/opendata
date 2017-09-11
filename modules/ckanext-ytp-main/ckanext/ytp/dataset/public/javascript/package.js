9c7c5a9cd90ff91f4d602e8d7a15550979da28c1
Jari Voutilainen <jari.voutilainen@iki.fi>
2017-09-11 09:24:20 +0300

    Remove draft/published controls from templates

--- modules/ckanext-ytp-main/ckanext/ytp/dataset/public/javascript/package.js
+++
@@ -1,8 +0,0 @@
-var SaveAsDraft = function(){
-    document.querySelector('#field-state').value = 'draft';
-}
-
-var SaveAsActive = function(){
-    document.querySelector('#field-state').value = 'active';
-}
-
