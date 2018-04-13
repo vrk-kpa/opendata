bb6f2bfd96c9d7b9fe53a5e8d820f3eff78be160
Teemu Erkkola <teemu.erkkola@gofore.com>
2018-04-13 14:42:05 +0300

    Translation fixes to dataset and resource pages, header

--- modules/ckanext-ytp_main/ckanext/ytp/plugin.py
+++ modules/ckanext-ytp_main/ckanext/ytp/plugin.py
@@ -25,7 +25,6 @@ from ckan.lib.plugins import DefaultOrganizationForm, DefaultTranslation
 from ckan.logic import NotFound, NotAuthorized, auth as ckan_auth, get_action, NotFound
 from ckan.model import Session
 from ckan.plugins import toolkit
-import ckan.lib.i18n as i18n
 from ckanext.harvest.model import HarvestObject
 from ckanext.report.interfaces import IReport
 from ckanext.spatial.interfaces import ISpatialHarvester
@@ -42,7 +41,7 @@ import menu
 import logic
 
 from converters import to_list_json, from_json_list, is_url, convert_to_tags_string, string_join, date_validator, simple_date_validate
-from helpers import extra_translation, render_date, get_dict_tree_from_json, service_database_enabled, get_json_value, sort_datasets_by_state_priority, get_facet_item_count, get_remaining_facet_item_count, sort_facet_items_by_name, get_sorted_facet_items_dict, calculate_dataset_stars, get_upload_size, get_license, get_visits_for_resource, get_visits_for_dataset, get_geonetwork_link, calculate_metadata_stars, get_tooltip_content_types, unquote_url, sort_facet_items_by_count, scheming_field_only_default_required, add_locale_to_source, scheming_language_text_or_empty, get_lang_prefix, call_toolkit_function
+from helpers import extra_translation, render_date, get_dict_tree_from_json, service_database_enabled, get_json_value, sort_datasets_by_state_priority, get_facet_item_count, get_remaining_facet_item_count, sort_facet_items_by_name, get_sorted_facet_items_dict, calculate_dataset_stars, get_upload_size, get_license, get_visits_for_resource, get_visits_for_dataset, get_geonetwork_link, calculate_metadata_stars, get_tooltip_content_types, unquote_url, sort_facet_items_by_count, scheming_field_only_default_required, add_locale_to_source, scheming_language_text_or_empty, get_lang_prefix, call_toolkit_function, get_translated, dataset_display_name, resource_display_name
 from tools import add_languages_modify, add_languages_show, add_translation_show_schema, add_translation_modify_schema, get_original_method, create_system_context, get_original_method, add_translation_show_schema, add_languages_show, add_translation_modify_schema, add_languages_modify
 
 from ckan.logic.validators import tag_length_validator, tag_name_validator
@@ -222,27 +221,6 @@ def action_package_show(context, data_dict):
 
     return result
  
-def get_translated(data_dict, field):
-    translated = data_dict.get('%s_translated' % field)
-    if isinstance(translated, dict):
-        language = i18n.get_lang()
-        if language in translated:
-            return translated[language]
-        dialects = [l for l in translated if l.startswith(language) or language.startswith(l)]
-        if dialects:
-            return translated[dialects[0]]
-    return data_dict.get(field)
-
-# Copied from core ckan to call over ridden get_translated
-def dataset_display_name(package_or_package_dict):
-    if isinstance(package_or_package_dict, dict):
-        return get_translated(package_or_package_dict, 'title') or \
-               package_or_package_dict['name']
-    else:
-        # FIXME: we probably shouldn't use the same functions for
-        # package dicts and real package objects
-        return package_or_package_dict.title or package_or_package_dict.name
-
 class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, YtpMainTranslation):
     plugins.implements(plugins.interfaces.IFacets, inherit=True)
     plugins.implements(plugins.IDatasetForm, inherit=True)
@@ -474,7 +452,7 @@ class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, YtpMai
         extra_dict = {}
         for key in extras:
             if key not in self._key_exclude:
-                value = extras.get(key)
+                value = get_translated(extras, key)
                 if value:
                     extra_dict.update({_prettify(key): value})
         return extra_dict
@@ -483,7 +461,7 @@ class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, YtpMai
         extra_dict = {}
         for key in extras:
             if key not in self._key_exclude_resources:
-                value = extras.get(key)
+                value = get_translated(extras, key)
                 if value:
                     extra_dict.update({_prettify(key): value})
         return extra_dict
@@ -535,7 +513,7 @@ class YTPDatasetForm(plugins.SingletonPlugin, toolkit.DefaultDatasetForm, YtpMai
 
     def _resource_display_name(self, resource_dict):
         """ taken from helpers.resource_display_name """
-        value = helpers.resource_display_name(resource_dict)
+        value = resource_display_name(resource_dict)
         return value if value != _("Unnamed resource") else _("Additional Info")
 
     def _auto_author_set(self):
