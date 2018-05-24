532c12f4db3cb2e0a7526ae5acea85da74b022a7
Pentti Laitinen <pentti.laitinen@gofore.com>
2018-05-24 14:55:15 +0300

    Added newsfeed module.
    - Added newsfeed module that displays recent news and events.
    - Added single server playbook for drupla 8 only (useful when one needs
    to provision only drupal related things).
    - Changed theme settings applied by ansible to use fluid container type
    by default.
    - New custom modules are now placed automatically to front page thanks
    to theme installation settings.
    - Added new content type "Avoindata Event".

---
+++ modules/avoindata-drupal-newsfeed/README.md
@@ -0,0 +1,3 @@
+# Avoindata news feed module #
+
+Avoindata news feed module.
---
+++ modules/avoindata-drupal-newsfeed/src/Plugin/Block/NewsFeedBlock.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Drupal\avoindata_newsfeed\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Entity\Query\QueryFactory;
+
+/**
+ * Provides a 'Avoindata news feed' Block.
+ *
+ * @Block(
+ *   id = "avoindata_newsfeed",
+ *   admin_label = @Translation("Avoindata news feed"),
+ *   category = @Translation("Avoindata news feed"),
+ * )
+ */
+class NewsFeedBlock extends BlockBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $articleNodeIds = \Drupal::entityQuery('node')
+    ->condition('type', 'article')
+    ->sort('created' , 'DESC')
+    ->range(0, 5)
+    ->execute();
+    $articleNodes = \Drupal::entityTypeManager()
+    ->getStorage('node')
+    ->loadMultiple($articleNodeIds);
+
+    // TODO: How will we sort the upcoming events?
+    $eventNodeIds = \Drupal::entityQuery('node')
+    ->condition('type', 'avoindata_event')
+    ->sort('created' , 'DESC')
+    ->range(0, 5)
+    ->execute();
+    $eventNodes = \Drupal::entityTypeManager()
+    ->getStorage('node')
+    ->loadMultiple($eventNodeIds);
+
+    return array(
+      '#newsfeed' => $articleNodes,
+      '#eventfeed' => $eventNodes,
+      '#language' => \Drupal::languageManager()->getCurrentLanguage()->getId(),
+      '#theme' => 'avoindata_newsfeed',
+    );
+  }
+}
