da8e79a84b51c58c7544aa9eb2b02ff0f288287c
Teemu Leivo <teemu.leivo@gofore.com>
2018-07-31 16:27:14 +0300

    Created menu for guides, but it's not visible on the site

---
+++ modules/avoindata-drupal-guide/src/Plugin/Menu/GuideMenuLink.php
@@ -0,0 +1,9 @@
+<?php
+namespace Drupal\avoindata_guide\Plugin\Menu;
+ 
+use Drupal\Core\Menu\MenuLinkDefault;
+ 
+/**
+ * Represents a menu link for a single Product.
+ */
+class GuideMenuLink extends MenuLinkDefault {}
