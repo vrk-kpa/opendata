1146e13212a5113ae086d4302780eee3c37b9b92
Teemu Leivo <teemu.leivo@gofore.com>
2018-08-12 12:41:20 +0300

    Removed unnecessary uid definitions and the dynamic guidemenulink implementation that didn't work

--- modules/avoindata-drupal-guide/src/Plugin/Menu/GuideMenuLink.php
+++
@@ -1,9 +0,0 @@
-<?php
-namespace Drupal\avoindata_guide\Plugin\Menu;
- 
-use Drupal\Core\Menu\MenuLinkDefault;
- 
-/**
- * Represents a menu link for a single Product.
- */
-class GuideMenuLink extends MenuLinkDefault {}
