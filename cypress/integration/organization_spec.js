01fa671b77e48dc2e5cad8c016661e7332b8cbe8
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-01-28 16:23:58 +0200

    Fix organization creation

---
+++ cypress/integration/organization_spec.js
@@ -0,0 +1,34 @@
+describe("Organization tests", function() {
+
+  beforeEach(function(){
+    const username = 'test-user';
+    const password = 'test-user';
+
+    cy.request({
+      method: 'POST',
+      url: '/user/login',
+      form: true,
+      body: {
+        name: username,
+        pass: password,
+        form_id: 'user_login_form'
+      }
+    });
+  });
+
+  it('Creates organization', function(){
+    cy.visit('/');
+    cy.get('nav a[href="/data/fi/organization"]').click();
+    cy.get('a[href="/data/fi/organization/new"]').click();
+
+    cy.get("input[name='title_translated-fi']").type('test organization');
+
+    cy.get('button').contains('Muokkaa').click();
+    cy.get("input[name='name']").type('test-organization');
+
+    cy.get('button[name="save"]').click();
+
+    cy.url().should('/data/fi/organization/test-organization');
+
+  })
+});
