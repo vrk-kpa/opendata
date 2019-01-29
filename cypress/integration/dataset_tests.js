5fe2bc9bccfef7c20463f2e2d7fae46f2a371c93
Teemu Leivo <teemu.leivo@gofore.com>
2019-01-29 17:53:18 +0200

    Basic tests for datasets and organizations

--- cypress/integration/dataset_tests.js
+++ cypress/integration/dataset_tests.js
@@ -1,36 +1,46 @@
-describe('New dataset tests', function() {
+describe('Dataset tests', function() {
 
     beforeEach(function () {
-        //cy.login('test_user');
         cy.login_post_request('test-user', 'test-user')
         cy.visit('/');
         cy.get('nav a[href="/data/fi/dataset"]').click();
       })
 
-    it('Create a new minimal dataset and edit it', function() {
+    it('Create a new minimal dataset, edit it and delete it', function() {
       // Dataset form
       cy.get('a[href="/data/fi/dataset/new"]').click();
-      const dataset_name = 'test_dataset_' + Math.random().toString(36).substring(7);
+
+      const random_id = Math.random().toString(36).substring(7);
+      const dataset_name = 'test_dataset_' + random_id;
       cy.get("input[id='field-title_translated-fi']").type(dataset_name);
   
       cy.get('.slug-preview button').contains('Muokkaa').click();
       cy.get('#field-name').type(dataset_name);
-      cy.get('#field-notes_translated-fi').type('Dataset test description')
-      cy.get('#s2id_autogen1').type('test_keyword {enter}')
-      cy.get('#field-maintainer').type('test maintainer')
-      cy.get('#field-maintainer_email').type('test.maintainer@example.com')
-      cy.get('button[name=save]').click()
+      cy.get('#field-notes_translated-fi').type('Dataset test description');
+      cy.get('#s2id_autogen1').type('test_keyword {enter}');
+      cy.get('#field-maintainer').type('test maintainer');
+      cy.get('#field-maintainer_email').type('test.maintainer@example.com');
+      cy.get('button[name=save]').click();
 
       // Resource form
-      cy.get('input[id=field-name_translated-fi]').type('test data')
-      cy.get('.form-actions button').contains('Valmis').click()
+      cy.get('input[id=field-name_translated-fi]').type('test data');
+      cy.get('.form-actions button').contains('Valmis').click();
+      cy.url().should('include', `/data/fi/dataset/${dataset_name}`);
 
       // Edit
-      cy.get(`a[href='/data/fi/dataset/edit/${dataset_name}']`).click()
-      cy.get(`input[id='field-title_translated-fi'][value='${dataset_name}'`).type('edit')
-      cy.get('button[name=save]').click()
-      cy.get('.dataset-title-column').contains(dataset_name+'edit')
-      cy.get(`a[href='/data/fi/dataset/edit/${dataset_name}']`) // Url shouldn't change 
+      cy.get(`a[href='/data/fi/dataset/edit/${dataset_name}']`).click();
+      cy.get(`input[id='field-title_translated-fi'][value='${dataset_name}'`).type('edit');
+      cy.get('button[name=save]').click();
+      cy.get('.dataset-title-column').contains(dataset_name+'edit');
+       
+
+      //Delete
+      cy.get(`a[href='/data/fi/dataset/edit/${dataset_name}']`).click(); // Url shouldn't have changed
+      cy.get('.form-actions').contains('Poista').click();
+      cy.get('#page_wrapper').find('.btn').contains('Vahvista').click();
+      cy.get('.search-input .search').type(random_id + '{enter}');
+      cy.get('.dataset-list').should('not.exist');
+      cy.contains("ei löytynyt tietoaineistoja");
 
 
     
@@ -40,15 +50,15 @@ describe('New dataset tests', function() {
   
     it('Creating an empty dataset fails', function() {
       cy.get('a[href="/data/fi/dataset/new"]').click();
-      cy.get('button[name=save]').click()
-      cy.get('.error-explanation') // Should result in an error message
+      cy.get('button[name=save]').click();
+      cy.get('.error-explanation');
 
     })
 
     it('Cannot create dataset if logged out', function() {
-      cy.logout()
+      cy.logout();
       cy.visit('/');
       cy.get('nav a[href="/data/fi/dataset"]').click();
-      cy.get('a[href="/data/fi/dataset/new"]').should('not.exist')
+      cy.get('a[href="/data/fi/dataset/new"]').should('not.exist');
     })
 })
--- cypress/integration/front_page_spec.js
+++ cypress/integration/front_page_spec.js
@@ -1,16 +1,16 @@
 describe('Basic tests', function() {
   it('Opens frontpage!', function() {
-    cy.visit("/")
+    cy.visit("/");
   });
 
   it('Opens dataset page!', function() {
-    cy.visit("/data/dataset")
-  })
+    cy.visit("/data/dataset");
+  });
 });
 
 describe('Login page', function(){
   it('Logs in', function(){
-    cy.login('admin', 'admin')
+    cy.login('admin', 'admin');
   })
   it('Logs out', function(){
     cy.login_post_request('admin', 'admin');
