6890e693a9d5c0eb6867224a177f2bf8e1212cd1
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-02-28 10:48:03 +0200

    Initial less linting

--- modules/ytp-assets-common/gulpfile.js
+++ modules/ytp-assets-common/gulpfile.js
@@ -15,10 +15,13 @@ var pump = require("pump");
 var npmDist = require('gulp-npm-dist');
 var rename = require('gulp-rename');
 var imageminJpegoptim = require('imagemin-jpegoptim');
+var lesshint = require('gulp-lesshint');
+
 
 var paths = {
   src: {
     images: "src/images/**/*",
+    less: "src/less",
     ckan: "src/less/ckan",
     drupal: "src/less/drupal/style.less",
     drupal_avoindata_header: "../avoindata-drupal-header/resources/avoindata_header.js",
@@ -51,6 +54,16 @@ gulp.task('copy:fontawesomeLess', (done) => {
   pump([
     gulp.src(fontawesomeLessPath + "/*.less"),
     gulp.dest(paths.src.root + "/vendor/@fortawesome/fontawesome/less")
+  ], done)
+});
+
+
+gulp.task("lint", (done) =>{
+  pump([
+    gulp.src([paths.src.less + "/*.less", paths.src.ckan + "/*.less", paths.src.drupal + "/*.less"]),
+    lesshint(),
+    lesshint.reporter(),
+    lesshint.failOnError()
   ], done)
 });
 
@@ -228,6 +241,7 @@ gulp.task(
     "clean",
     "config",
     "copy:fontawesomeLess",
+    "lint",
     gulp.parallel(
       "bootstrap_styles",
       "bootstrap_scripts",
