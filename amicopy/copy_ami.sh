008d0730c0b8305e0aa7d3e444294ad2a461a9a0
Juho Majasaari <juho.majasaari@gofore.com>
2019-04-02 09:28:27 +0300

    AV-638: Fix env vars in script

--- amicopy/copy_ami.sh
+++ amicopy/copy_ami.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-assumed_build_role=$(aws sts assume-role --role-session-name 'build-assumed-role' --role-arn arn:aws:iam::${buildtest_account_id}:role/InvokeAmiCopyFunctionRole)
+assumed_build_role=$(aws sts assume-role --role-session-name 'build-assumed-role' --role-arn arn:aws:iam::${AWS_BUILDTEST_ACCOUNT_ID}:role/InvokeAmiCopyFunctionRole)
 export AWS_ACCESS_KEY_ID=$(echo $assumed_build_role | jq .Credentials.AccessKeyId |xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $assumed_build_role | jq .Credentials.SecretAccessKey |xargs)
 export AWS_SESSION_TOKEN=$(echo $assumed_build_role | jq .Credentials.SessionToken |xargs)
-production_copy_role=$(aws sts assume-role --role-session-name 'build-assumed-role' --role-arn arn:aws:iam::${production_account_id}:role/CopyAmiLambdaExecutionRole)
+production_copy_role=$(aws sts assume-role --role-session-name 'build-assumed-role' --role-arn arn:aws:iam::${AWS_PRODUCTION_ACCOUNT_ID}:role/CopyAmiLambdaExecutionRole)
 export AWS_ACCESS_KEY_ID=$(echo $production_copy_role | jq .Credentials.AccessKeyId |xargs)
 export AWS_SECRET_ACCESS_KEY=$(echo $production_copy_role | jq .Credentials.SecretAccessKey |xargs)
 export AWS_SESSION_TOKEN=$(echo $production_copy_role | jq .Credentials.SessionToken |xargs)
