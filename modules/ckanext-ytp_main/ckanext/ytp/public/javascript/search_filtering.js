e50cae0996c8edad82dcf46f61410591a75c9b72
Teemu Leivo <teemu.leivo@gofore.com>
2019-05-02 18:10:16 +0300

    Different icon and text for hide button

--- modules/ckanext-ytp_main/ckanext/ytp/public/javascript/search_filtering.js
+++ modules/ckanext-ytp_main/ckanext/ytp/public/javascript/search_filtering.js
@@ -1,27 +1,38 @@
-document.getElementById("search-filter-toggle-button").addEventListener("click", toggle_search_filters);
-document.getElementById("search-filter-upper-toggle-button").addEventListener("click", toggle_search_filters);
+document.getElementById("search-filter-show-button").addEventListener("click", toggle_search_filters);
+document.getElementById("search-filter-upper-hide-button").addEventListener("click", toggle_search_filters);
+document.getElementById("search-filter-lower-hide-button").addEventListener("click", toggle_search_filters);
 
-// If url parameter exists, the search filters and the second toggle button are shown. 
+// If url parameter exists, the search filters and both 'hide filters' buttons are shown. 
 if (window.location.search.indexOf("_show_search_filters=true") > -1) {
   var search_filters = document.getElementsByClassName("sidebar-offcanvas")[0];
-  var upper_toggle_button = document.getElementById("search-filter-upper-toggle-button");
+  var upper_hide_button = document.getElementById("search-filter-upper-hide-button");
+  var lower_hide_button = document.getElementById("search-filter-lower-hide-button");
+  var show_button = document.getElementById("search-filter-show-button");
   search_filters.classList.remove("search-filters-hidden");
   search_filters.classList.add("search-filters-visible");
-  upper_toggle_button.classList.add("upper-toggle-button-visible");
+  upper_hide_button.classList.add("hide-button-visible");
+  lower_hide_button.classList.add("hide-button-visible");
+  show_button.classList.add("show-button-hidden");
 }
 
 function toggle_search_filters() {
   var search_filters = document.getElementsByClassName("sidebar-offcanvas")[0];
-  var upper_toggle_button = document.getElementById("search-filter-upper-toggle-button");
+  var upper_hide_button = document.getElementById("search-filter-upper-hide-button");
+  var lower_hide_button = document.getElementById("search-filter-lower-hide-button");
+  var show_button = document.getElementById("search-filter-show-button");
   if (search_filters.classList.contains("search-filters-hidden")) {
     search_filters.classList.remove("search-filters-hidden");
     search_filters.classList.add("search-filters-visible");
-    upper_toggle_button.classList.add("upper-toggle-button-visible");
+    upper_hide_button.classList.add("hide-button-visible");
+    lower_hide_button.classList.add("hide-button-visible");
+    show_button.classList.add("show-button-hidden");
     updateURLParameter("_show_search_filters", "true");
   } else {
     search_filters.classList.remove("search-filters-visible");
     search_filters.classList.add("search-filters-hidden");
-    upper_toggle_button.classList.remove("upper-toggle-button-visible");
+    upper_hide_button.classList.remove("hide-button-visible");
+    lower_hide_button.classList.remove("hide-button-visible");
+    show_button.classList.remove("show-button-hidden");
     updateURLParameter("_show_search_filters", "false");
   }
 }
