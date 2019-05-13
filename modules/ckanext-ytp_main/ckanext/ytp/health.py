6173e65f43f3165c5f0b931bc820c62e8d017a75
Juho Majasaari <juho.majasaari@gofore.com>
2019-05-13 15:15:10 +0300

    Fix style

--- modules/ckanext-ytp_main/ckanext/ytp/health.py
+++ modules/ckanext-ytp_main/ckanext/ytp/health.py
@@ -2,13 +2,12 @@ import ckan.lib.base as base
 import pylons.config as config
 import urllib2
 import logging
-import datetime
-from pprint import pformat
 log = logging.getLogger(__name__)
 
 SITE_URL_FAILURE_LOGMESSAGE = "Site URL '%s' failed to respond during health check."
 FAILURE_MESSAGE = "An error has occurred, check the server log for details"
 SUCCESS_MESSAGE = "OK"
+
 
 def check_url(url):
     try:
