4702ef8b6b9234ed411a55eedb24d2894e4be92a
Tuomas Vallaskangas <tuomas.vallaskangas@gofore.com>
2019-05-22 14:17:32 +0300

    AV-539: Remove quotation marks from ami id files - Remove obsolete chmod commands - Fix assume_role.sh script paths - Improve cloudformation stack status checks - Fix syntax for updating parameters and add validation pattern - Add checkout step to infratest-deploy job

--- deploy/assume_role.sh
+++ deploy/assume_role.sh
@@ -12,7 +12,7 @@ export AWS_SECRET_ACCESS_KEY=$(echo $temp_buildtest_role | jq .Credentials.Secre
 export AWS_SESSION_TOKEN=$(echo $temp_buildtest_role | jq .Credentials.SessionToken | xargs)
 
 temp_deploy_role=$(aws sts assume-role \
-                    --role-arn "arn:aws:iam::${AWS_PRODUCTION_ACCOUNT_ID}:role/InvokeDeploymentRole" \
+                    --role-arn "arn:aws:iam::${AWS_PRODUCTION_ACCOUNT_ID}:role/DeploymentRole" \
                     --role-session-name "deploy-assumed-role")
 
 export AWS_ACCESS_KEY_ID=$(echo $temp_deploy_role | jq .Credentials.AccessKeyId | xargs)
