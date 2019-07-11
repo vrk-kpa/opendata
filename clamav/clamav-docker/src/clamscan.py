228817c7097444e60fd862b527a9018b33c05503
Tuomas Vallaskangas <tuomas.vallaskangas@gofore.com>
2019-07-11 14:13:13 +0300

    AV-840: Add sha256 calculation - Remove S3 related functions from clamscan class

--- clamav/clamav-docker/src/clamscan.py
+++ clamav/clamav-docker/src/clamscan.py
@@ -1,13 +1,9 @@
-from botocore.exceptions import ClientError
-import boto3
 import clamd
-import json
 import logging
 import subprocess
 import time
 
 logger = logging.getLogger(__name__)
-
 
 def clamd_check(decorated):
     def wrapper(*args):
@@ -20,18 +16,12 @@ def clamd_check(decorated):
     return wrapper
 
 
-class ClamS3Scanner:
+class ClamScanner:
     clamd_up = False
-    s3_client = boto3.client('s3')
-    sns_client = boto3.client('sns')
     clamd_client = clamd.ClamdUnixSocket()
 
-    def __init__(self, s3_bucket, object_key, sns_topic_arn=None):
-        self.s3_bucket = s3_bucket
-        self.object_key = object_key
-        self.s3_object_client = boto3.resource(
-            's3').Object(s3_bucket, object_key)
-        self.sns_topic_arn = sns_topic_arn
+    def __init__(self, file_name):
+        self.file = file_name
         self.update_clamav_definitions()
         self.start_clam_daemon()
 
@@ -78,63 +68,12 @@ class ClamS3Scanner:
     @clamd_check
     def scan_file(self):
         try:
-            file_name = self.object_key.split('/')[-1]
-            scan_target_path = f'/tmp/{file_name}'
-            self.s3_object_client.download_file(scan_target_path)
+            scan_target_path = self.file
             logger.info(
-                f'Starting scan of {self.object_key} in {self.s3_bucket}')
+                f'Starting scan of {self.file}')
             scan_output = self.clamd_client.scan(scan_target_path)
             return scan_output
-        except ClientError as e:
-            logger.error(
-                f'Downloading {self.object_key} from {self.s3_bucket} failed: \n{e}')
-            raise e
+
         except clamd.ClamdError as e:
             logger.error(f'Clamd scan failed: \n{e}')
-            raise e
-
-    def sns_publish(self, infection_name):
-        message = {
-            "bucket_name": self.s3_bucket,
-            "object_key": self.object_key,
-            "infection_name": infection_name
-        }
-
-        try:
-            self.sns_client.publish(
-                Subject='Virus found',
-                TopicArn=self.sns_topic_arn,
-                Message=json.dumps(message)
-            )
-            logger.info('Message published to SNS')
-        except ClientError as e:
-            logger.error(f'Message publishing failed: \n{e}')
-
-    def tag_object(self, tag_value):
-        s3_tags = {
-            'TagSet': [
-                {
-                    'Key': 'Status',
-                    'Value': tag_value
-                }
-            ]
-        }
-
-        try:
-            self.s3_client.put_object_tagging(
-                Bucket=self.s3_bucket, Key=self.object_key, Tagging=s3_tags)
-            logger.info(f'Tagged {self.object_key} with Status: {tag_value}')
-        except ClientError as e:
-            logger.error(f'Object tagging failed: \n{e}')
-            raise e
-
-    def delete_object(self):
-        try:
-            logger.info(
-                f'Setting delete marker for {self.object_key} in {self.s3_bucket}')
-            self.s3_object_client.delete()
-            logger.info(f'Delete marker set for {self.object_key}')
-        except Exception as e:
-            logger.error(
-                f'Delete marker setting failed for {self.object_key}: \n{e}')
             raise e
