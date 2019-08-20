6001507022305b451097dab488f9543e7d25dd1c
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-08-20 11:59:59 +0300

    AV-1003: Add lazy loading to applist

--- modules/avoindata-drupal-appfeed/src/Plugin/Block/AppFeedBlock.php
+++ modules/avoindata-drupal-appfeed/src/Plugin/Block/AppFeedBlock.php
@@ -3,7 +3,6 @@
 namespace Drupal\avoindata_appfeed\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Component\Serialization\Json;
 
 /**
  * Provides a 'Avoindata App Feed' Block.
@@ -20,21 +19,15 @@ class AppFeedBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $client = \Drupal::httpClient();
-    $currentLang = \Drupal::languageManager()->getCurrentLanguage()->getId();
 
-    try {
-      $recentApplicationsResponse = $client->request('GET', 'http://localhost:8080/data/api/action/package_search?fq=dataset_type:showcase&sort=metadata_modified%20desc&rows=3');
-      $recentApplicationsResult = Json::decode($recentApplicationsResponse->getBody());
-      $recentApplications = $recentApplicationsResult['result']['results'];
-    }
-    catch (\Exception $e) {
-      $recentApplications = NULL;
-    }
+    $recentApplications = [
+      '#lazy_builder' => ['avoindata_recent_applications', []],
+      '#create_placeholder' => FALSE,
+    ];
 
     return [
       '#applications' => $recentApplications,
-      '#language' => $currentLang,
+      '#language' => \Drupal::languageManager()->getCurrentLanguage()->getId(),
       '#theme' => 'avoindata_appfeed',
     ];
   }
