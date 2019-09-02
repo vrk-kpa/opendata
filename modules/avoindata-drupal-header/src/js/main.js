189ba5a6de447d2f7b7684002bc8aa4d38755c34
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-09-02 13:32:30 +0300

    AV-1056: implement new language switcher

--- modules/avoindata-drupal-header/src/js/main.js
+++ modules/avoindata-drupal-header/src/js/main.js
@@ -21,3 +21,8 @@ function onHeaderSubmitClick(event) {
     searchInputContainer.classList.toggle('is-hidden');
   }
 }
+
+global.toggleLanguageDropdown = function() {
+  document.getElementsByClassName('dropdown-button')[0].classList.toggle("dropdown-open");
+  document.getElementsByClassName('dropdown-content')[0].classList.toggle("dropdown-open");
+};
