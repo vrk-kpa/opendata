c2b6ea29372fd2000d6f72d8e4a6afa8fc19e93d
Teemu Erkkola <teemu.erkkola@gofore.com>
2019-09-03 10:38:39 +0300

    AV-986: Added dct:publisher and dcat:theme properties to dataset DCAT-AP representation

--- modules/ckanext-ytp_main/ckanext/ytp/dcat.py
+++ modules/ckanext-ytp_main/ckanext/ytp/dcat.py
@@ -1,9 +1,8 @@
 from rdflib import URIRef, BNode, Literal
-from rdflib.namespace import Namespace, RDF
-from ckanext.dcat.profiles import RDFProfile, VCARD, DCAT
+from rdflib.namespace import RDF
+from ckanext.dcat.profiles import RDFProfile, VCARD, DCAT, DCT, FOAF, SKOS
 from ckanext.dcat.utils import resource_uri
-
-DCT = Namespace("http://purl.org/dc/terms/")
+from ckan.plugins import toolkit as p
 
 
 class AvoindataDCATAPProfile(RDFProfile):
@@ -12,7 +11,8 @@ class AvoindataDCATAPProfile(RDFProfile):
 
     Supported dataset fields:
         Mandatory: dct:title, dct:description
-        Optional:  dcat:contactPoint, dcat:distribution, dcat:keyword
+        Recommended:  dcat:contactPoint, dcat:distribution, dcat:keyword
+                      dct:publisher, dcat:theme
     '''
 
     #
@@ -77,3 +77,31 @@ class AvoindataDCATAPProfile(RDFProfile):
 
         for keyword in keywords:
             g.add((dataset_ref, DCAT.keyword, Literal(keyword)))
+
+        # dct:publisher
+        context = {'user': p.c.user}
+        organization = p.get_action('organization_show')(context, data_dict={'id': dataset_dict['owner_org']})
+
+        publisher = URIRef(p.url_for(controller='organization', action='read', id=organization['id'], qualified=True))
+        g.add((publisher, RDF.type, FOAF.Organization))
+        g.add((dataset_ref, DCT.publisher, publisher))
+
+        organization_titles = (t for t in organization.get('title_translated', {}).values() if t)
+
+        for title in organization_titles:
+            g.add((publisher, FOAF.name, Literal(title)))
+
+        self._add_triple_from_dict(organization, publisher, FOAF.homepage, 'homepage')
+
+        # dcat:theme
+        groups = dataset_dict.get('groups', [])
+
+        for group_item in groups:
+            group_dict = p.get_action('group_show')(context, data_dict={'id': group_item['id']})
+            theme = URIRef(p.url_for(controller='group', action='read', id=group_dict['id'], qualified=True))
+            g.add((theme, RDF.type, SKOS.Concept))
+            g.add((dataset_ref, DCAT.theme, theme))
+
+            group_titles = (t for t in group_dict.get('title_translated', {}).values() if t)
+            for title in group_titles:
+                g.add((theme, SKOS.prefLabel, Literal(title)))
