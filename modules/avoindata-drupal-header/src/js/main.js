8edeaf2847b93b97e8c72cd87448152e34ab3afb
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-09-05 11:35:58 +0300

    AV-1056: remove search element javascript

--- modules/avoindata-drupal-header/src/js/main.js
+++ modules/avoindata-drupal-header/src/js/main.js
@@ -1,27 +1,3 @@
-document.addEventListener('readystatechange', function () {
-  if (document.readyState === 'interactive') {
-    // If client is not blocking js we show the search inputs
-    document.getElementById('header-search-container').hidden = false;
-    addHeaderSearchListeners();
-  }
-});
-
-function addHeaderSearchListeners() {
-  const searchSubmit = document.getElementById('avoindata-nav-search-submit');
-  searchSubmit.onclick = onHeaderSubmitClick;
-}
-
-function onHeaderSubmitClick(event) {
-  const searchInput = document.getElementById('avoindata-nav-search-input');
-  const searchInputContainer = document.getElementsByClassName('header-search-input-container')[0];
-  const searchForm = document.getElementById('header-search-form');
-  if(searchInput.value.length > 0 && searchForm) {
-    searchForm.submit();
-  } else {
-    searchInputContainer.classList.toggle('is-hidden');
-  }
-}
-
 global.toggleLanguageDropdown = function() {
   document.getElementsByClassName('dropdown-button')[0].classList.toggle("dropdown-open");
   document.getElementsByClassName('dropdown-content')[0].classList.toggle("dropdown-open");
