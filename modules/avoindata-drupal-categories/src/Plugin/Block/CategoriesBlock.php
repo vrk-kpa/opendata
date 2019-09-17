eb34f30e71be0206a3fea314cec812fe8e8dfbc7
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-09-17 13:38:48 +0300

    AV-1036: Use lazy loading for frontpage category list

--- modules/avoindata-drupal-categories/src/Plugin/Block/CategoriesBlock.php
+++ modules/avoindata-drupal-categories/src/Plugin/Block/CategoriesBlock.php
@@ -3,7 +3,6 @@
 namespace Drupal\avoindata_categories\Plugin\Block;
 
 use Drupal\Core\Block\BlockBase;
-use Drupal\Component\Serialization\Json;
 
 /**
  * Provides a 'Avoindata Categories' Block.
@@ -20,21 +19,15 @@ class CategoriesBlock extends BlockBase {
    * {@inheritdoc}
    */
   public function build() {
-    $client = \Drupal::httpClient();
-
-    try {
-      $response = $client->request('GET', 'http://localhost:8080/data/api/3/action/group_list?all_fields=true&include_extras=true');
-      $result = Json::decode($response->getBody());
-      $categories = $result['result'];
-    }
-    catch (\Exception $e) {
-      $categories = NULL;
-    }
+    $categories = [
+      '#lazy_builder' => ['avoindata_categories', []],
+      '#create_placeholder' => FALSE,
+    ];
 
     return [
-      '#theme' => 'avoindata_categories',
       '#categories' => $categories,
       '#language' => \Drupal::languageManager()->getCurrentLanguage()->getId(),
+      '#theme' => 'avoindata_categories',
     ];
   }
 
