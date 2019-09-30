d0283abfb259a4a4894defa4abc2801c8a351070
Eetu Mansikkamäki <eetu.mansikkamaki@gofore.com>
2019-09-30 14:27:55 +0300

    AV-1046: Add custom note widget

--- modules/avoindata-drupal-ckeditor-plugins/js/plugin.js
+++ modules/avoindata-drupal-ckeditor-plugins/js/plugin.js
@@ -20,7 +20,6 @@
       editor.config.allowedContent = true;
       CKEDITOR.dtd.$removeEmpty.i = 0;
 
-
       editor.widgets.add( 'avoindata_expander', {
         template:
           '<div class="avoindata-expander" onclick="toggleAvoindataExpander(this)">' +
@@ -28,7 +27,7 @@
               '<h2 class="avoindata-expander-title">Title</h2>' +
               '<span class="icon-wrapper pull-right"><i class="fas fa-angle-down"></i></span>' +
             '</div>' +
-            '<div class="avoindata-expander-content collapse"><p>Content...</p></div>' +
+            '<div class="avoindata-expander-content collapse">Content</div>' +
           '</div>',
 
         editables: {
@@ -54,6 +53,57 @@
 
         // The path to the icon.
         icon: this.path + '../icons/avoindata_expander.png'
+      });
+    }
+  });
+  // Register the plugin within the editor.
+  CKEDITOR.plugins.add('avoindata_note', {
+    requires: 'widget',
+
+    // Register the icons.
+    icons: 'avoindata_note',
+
+    // The plugin initialization logic goes inside this method.
+    init: function (editor) {
+      editor.addContentsCss( this.path + '../css/style.css' );
+
+      // Allow any attributes.
+      editor.config.allowedContent = true;
+      CKEDITOR.dtd.$removeEmpty.i = 0;
+
+      editor.widgets.add( 'avoindata_note', {
+        template:
+          '<div class="avoindata-note">' +
+            '<div class="avoindata-note-header">' +
+              '<img class="avoindata-header-image" src="/resources/images/avoindata-note-icon.svg"/>' +
+              '<h2 class="avoindata-note-title">Title</h2>' +
+            '</div>' +
+            '<div class="avoindata-note-content">Content</div>' +
+          '</div>',
+
+        editables: {
+          title: {
+              selector: '.avoindata-note-title'
+          },
+          content: {
+              selector: '.avoindata-note-content'
+          }
+        },
+
+        requiredContent: 'div(avoindata-note) h2(avoindata-note-title) div(avoindata-note-content)',
+
+        upcast: function( element ) {
+            return element.name == 'div' && element.hasClass( 'avoindata-note' );
+        }
+      });
+
+      // Create a toolbar button that executes the above command.
+      editor.ui.addButton('avoindata_note', {
+        // The command to execute on click.
+        command: 'avoindata_note',
+
+        // The path to the icon.
+        icon: this.path + '../icons/avoindata_note.png'
       });
     }
   });
