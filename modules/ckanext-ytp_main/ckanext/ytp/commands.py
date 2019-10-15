560309b3d81cc9f4a00e64f79188fd65271c1b94
Teemu Erkkola <teemu.erkkola@gofore.com>
2019-10-15 13:26:55 +0300

    AV-1083: More complete context for validators

--- modules/ckanext-ytp_main/ckanext/ytp/commands.py
+++ modules/ckanext-ytp_main/ckanext/ytp/commands.py
@@ -404,7 +404,8 @@ def validate(ctx, config, verbose):
     load_config(config or ctx.obj['config'])
 
     no_errors = True
-
+    user = t.get_action('get_site_user')({'model': model, 'ignore_auth': True}, {})
+    context = {'model': model, 'session': model.Session, 'user': user['name'], 'ignore_auth': True}
     for package_dict in package_generator('*:*', 1000):
         if verbose:
             print "Validating %s" % package_dict['name']
@@ -423,7 +424,7 @@ def validate(ctx, config, verbose):
             package_plugin = lib_plugins.lookup_package_plugin(package_dict['type'])
 
         schema = package_plugin.update_package_schema()
-        context = {'ignore_auth': True}
+
         data, errors = lib_plugins.plugin_validate(
             package_plugin, context, package_dict, schema, 'package_update')
 
