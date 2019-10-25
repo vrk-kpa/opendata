40a4fb1202e64bba4aad46076903336a0f6abfbf
jukka.luotonen@q-factory.fi <jukka.luotonen@q-factory.fi>
2019-10-25 10:24:45 +0300

    Added some page loads to test set

--- cypress/integration/front_page_spec.js
+++ cypress/integration/front_page_spec.js
@@ -7,24 +7,33 @@ describe('Basic tests', function() {
   // or other item to exist. Only tests that the url doesn't return an error code
   it('Opens all kinds of various pages', function() {
     cy.visit("/data/dataset");
+    cy.visit("/data/sv/dataset");
     cy.visit("/data/en_GB/dataset");
-    cy.visit("/data/dataset?tags=test")
+    cy.visit("/data/dataset?tags=test");
     cy.visit("/data/organization");
+    cy.visit("/data/fi/organization");
     cy.visit("/data/sv/organization");
+    cy.visit("/data/en_GB/organization");
     cy.visit("/data/showcase");
-    cy.visit("/data/submit-showcase")
+    cy.visit("/data/fi/showcase");
+    cy.visit("/data/sv/showcase");
+    cy.visit("/data/en_GB/showcase");
+    cy.visit("/data/submit-showcase");
     cy.visit("/data/group");
     cy.request("/data/api/3");
     cy.visit("/artikkelit");
     cy.visit("/en/articles");
-    cy.visit("/sv/artiklar")
+    cy.visit("/sv/artiklar");
+    cy.visit("/fi/artikkelit");
     cy.visit("/opas");
+    cy.visit("/fi/opas");
     cy.visit("/en/guide");
     cy.visit("/sv/guide");
     cy.visit("fi/guide-search?search_api_fulltext=test");
     cy.visit("/user/register");
     cy.visit("/contact");
-    cy.visit("/tapahtumat")
+    cy.visit("/tapahtumat");
+    cy.visit("/fi/tapahtumat");
     cy.visit("/en/events");
     cy.visit("/sv/evenemanger");
   })
