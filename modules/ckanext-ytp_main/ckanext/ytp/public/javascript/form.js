d89ef36b7ec65b598f14ae018e0725a899991d9e
Jari Voutilainen <jari.voutilainen@iki.fi>
2019-11-18 10:01:51 +0200

    AV-1115: Added missing javascript to calculate filesize and test to assert that it is stored in database

--- modules/ckanext-ytp_main/ckanext/ytp/public/javascript/form.js
+++ modules/ckanext-ytp_main/ckanext/ytp/public/javascript/form.js
@@ -65,3 +65,22 @@ function createRemoveLink(inputContainer) {
     inputContainer.append(removeLink);
     return removeLink;
 }
+
+
+// Calculate file size on resource submit.
+$(function(){
+  $( "#resource-edit" ).one('submit', function() {
+    var fileInput = $('#resource-edit input:file').get(0);
+    if(fileInput.files.length > 0) {
+      var fileSize = fileInput.files[0].size;
+      $('#field-file_size').val(fileSize);
+      var html = $('<div class="upload-times"><ul>' +
+        '<li>24/1 Mbit/s: ' + Math.ceil(fileSize / 125000 / 60) + ' min</li>' +
+        '<li>10/10 Mbit/s: ' + Math.ceil(fileSize / 1250000 / 60) + ' min</li>' +
+        '<li>100/100 Mbit/s: ' + Math.ceil(fileSize / 12500000 / 60) + ' min</li>' +
+        '</ul></div>');
+
+      $("#submit-info").append(html).show();
+    }
+  });
+});
