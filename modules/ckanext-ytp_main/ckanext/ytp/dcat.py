817c329608ad1264d954c07e340edadf7437f87f
Teemu Erkkola <teemu.erkkola@gofore.com>
2019-11-20 13:58:09 +0200

    AV-988: Fix catalog DCAT-AP representation to pass DCAT-AP 2.0 validation

--- modules/ckanext-ytp_main/ckanext/ytp/dcat.py
+++ modules/ckanext-ytp_main/ckanext/ytp/dcat.py
@@ -1,3 +1,4 @@
+import json
 from rdflib import URIRef, BNode, Literal
 from rdflib.namespace import RDF, XSD
 from ckanext.dcat.profiles import RDFProfile, VCARD, DCAT, DCT, FOAF, SKOS, ADMS, SPDX
@@ -14,6 +15,19 @@ namespaces = {
     'xsd': XSD,
     'spdx': SPDX
 }
+
+
+def as_dict(value):
+    if type(value) is dict:
+        return value
+    elif type(value) in (str, unicode):
+        return as_dict(json.loads(value))
+    else:
+        raise ValueError()
+
+
+def get_dict(d, key):
+    return as_dict(d.get(key, {}))
 
 
 class AvoindataDCATAPProfile(RDFProfile):
@@ -39,7 +53,6 @@ class AvoindataDCATAPProfile(RDFProfile):
         return dataset_dict
 
     def graph_from_dataset(self, dataset_dict, dataset_ref):
-
         g = self.g
 
         for prefix, namespace in namespaces.iteritems():
@@ -47,13 +60,19 @@ class AvoindataDCATAPProfile(RDFProfile):
         g.add((dataset_ref, RDF.type, DCAT.Dataset))
 
         # dct:title
-        titles = (t for t in self._get_dataset_value(dataset_dict, 'title_translated').values() if t)
+        titles = set(t for t in get_dict(dataset_dict, 'title_translated').values() if t)
+
+        if not titles:
+            titles.add(dataset_dict.get('title', ''))
 
         for title in titles:
             g.add((dataset_ref, DCT.title, Literal(title)))
 
         # dct:description
-        descriptions = (d for d in self._get_dataset_value(dataset_dict, 'notes_translated').values() if d)
+        descriptions = set(d for d in get_dict(dataset_dict, 'notes_translated').values() if d)
+
+        if not descriptions:
+            descriptions.add(dataset_dict.get('notes', ''))
 
         for description in descriptions:
             g.add((dataset_ref, DCT.description, Literal(description)))
@@ -71,39 +90,54 @@ class AvoindataDCATAPProfile(RDFProfile):
         self._add_triple_from_dict(dataset_dict, contact_details, VCARD.hasUrl, 'maintainer_website', _type=URIRef)
 
         # dcat:distribution
-        for resource_dict in self._get_dataset_value(dataset_dict, 'resources'):
+        for resource_dict in dataset_dict.get('resources', []):
+            resource_dict = as_dict(resource_dict)
             distribution = BNode()
             g.add((distribution, RDF.type, DCAT.Distribution))
             g.add((dataset_ref, DCAT.distribution, distribution))
 
             # dct:title
-            titles = (t for t in set(resource_dict.get('name_translated').values()) if t)
+            titles = set(t for t in get_dict(resource_dict, 'name_translated').values() if t)
+
+            if not titles:
+                titles.add(resource_dict.get('title', ''))
+
             for title in titles:
                 g.add((distribution, DCT.title, Literal(title)))
 
             # dct:description
-            descriptions = (d for d in set(resource_dict.get('description_translated').values()) if d)
+            descriptions = set(d for d in get_dict(resource_dict, 'description_translated').values() if d)
+
+            if not descriptions:
+                descriptions.add(dataset_dict.get('description', ''))
+
             for description in descriptions:
                 g.add((distribution, DCT.description, Literal(description)))
 
             # dct:rights
-            rights_statements = (n for n in set(dataset_dict.get('copyright_notice_translated').values()) if n)
-            for rights_statement in rights_statements:
-                g.add((distribution, DCT.rights, Literal(rights_statement)))
+            copyright_notices = set(n for n in get_dict(dataset_dict, 'copyright_notice_translated').values() if n)
+
+            if copyright_notices:
+                rights_statement = BNode()
+                g.add((rights_statement, RDF.type, DCT.RightsStatement))
+                g.add((rights_statement, DCT.description, Literal('\n\n'.join(copyright_notices))))
+                g.add((distribution, DCT.rights, rights_statement))
 
             # dcat:accessUrl
-            g.add((distribution, DCAT.accessUrl, URIRef(resource_uri(resource_dict))))
+            g.add((distribution, DCAT.accessURL, URIRef(resource_uri(resource_dict))))
 
             # dcat:downloadUrl
             resource_url = resource_dict.get('url')
             if resource_url:
-                g.add((distribution, DCAT.downloadUrl, URIRef(resource_url)))
+                g.add((distribution, DCAT.downloadURL, URIRef(resource_url)))
 
             # adms:status
             maturity = resource_dict.get('maturity')
 
             if maturity:
-                status = Literal(maturity)
+                status = BNode()
+                g.add((status, RDF.type, SKOS.Concept))
+                g.add((status, SKOS.prefLabel, Literal(maturity)))
                 g.add((distribution, ADMS.status, status))
 
             # dct:license
@@ -111,37 +145,46 @@ class AvoindataDCATAPProfile(RDFProfile):
 
             if license_url:
                 license_ref = URIRef(license_url)
+                g.add((license_ref, RDF.type, DCT.LicenseDocument))
                 g.add((distribution, DCT.license, license_ref))
 
             # dct:format
             file_format = resource_dict.get('format')
 
             if file_format:
-                media_type = Literal(file_format)
+                media_type = BNode()
+                g.add((media_type, RDF.type, DCT.MediaTypeOrExtent))
+                g.add((media_type, RDF.value, Literal(file_format)))
                 g.add((distribution, DCT['format'], media_type))
 
             # dcat:byteSize
             file_size = resource_dict.get('file_size')
 
             if file_size:
-                g.add((distribution, DCAT.byteSize, Literal(file_size)))
+                g.add((distribution, DCAT.byteSize, Literal(file_size, datatype=XSD.decimal)))
 
             # spdx:checksum
-            checksum = resource_dict.get('sha256')
+            checksum_value = resource_dict.get('sha256')
 
-            if checksum:
-                g.add((distribution, SPDX.checksum, Literal(checksum)))
+            if checksum_value:
+                checksum = BNode()
+                g.add((checksum, RDF.type, SPDX.Checksum))
+                g.add((checksum, SPDX.checksumValue, Literal(checksum_value)))
+                g.add((checksum, SPDX.algorithm, SPDX.checksumAlgorithm_sha1))
+                g.add((distribution, SPDX.checksum, checksum))
 
             # dcat:temporalResolution
-            temporal_granularities = set(t for lang in resource_dict.get('temporal_granularity').values() for t in lang if t)
+            temporal_granularities = set(t for lang in get_dict(resource_dict, 'temporal_granularity').values()
+                                         for t in lang if t)
 
-            for temporal_granularity in temporal_granularities:
-                g.add((distribution, DCAT.temporalResolution, Literal(temporal_granularity)))
+            if temporal_granularities:
+                g.add((distribution, DCAT.temporalResolution,
+                       Literal(', '.join(temporal_granularities), datatype=XSD.duration)))
 
         # dcat:keyword
         keywords = set(
                 keyword
-                for keyword_language in dataset_dict.get('keywords', {}).values()
+                for keyword_language in get_dict(dataset_dict, 'keywords').values()
                 for keyword in keyword_language)
 
         for keyword in keywords:
@@ -152,10 +195,10 @@ class AvoindataDCATAPProfile(RDFProfile):
         organization = p.get_action('organization_show')(context, data_dict={'id': dataset_dict['owner_org']})
 
         publisher = URIRef(p.url_for(controller='organization', action='read', id=organization['id'], qualified=True))
-        g.add((publisher, RDF.type, FOAF.Organization))
+        g.add((publisher, RDF.type, FOAF.Agent))
         g.add((dataset_ref, DCT.publisher, publisher))
 
-        organization_titles = (t for t in organization.get('title_translated', {}).values() if t)
+        organization_titles = (t for t in get_dict(organization, 'title_translated').values() if t)
 
         for title in organization_titles:
             g.add((publisher, FOAF.name, Literal(title)))
@@ -171,7 +214,7 @@ class AvoindataDCATAPProfile(RDFProfile):
             g.add((theme, RDF.type, SKOS.Concept))
             g.add((dataset_ref, DCAT.theme, theme))
 
-            group_titles = (t for t in group_dict.get('title_translated', {}).values() if t)
+            group_titles = (t for t in get_dict(group_dict, 'title_translated').values() if t)
             for title in group_titles:
                 g.add((theme, SKOS.prefLabel, Literal(title)))
 
@@ -193,13 +236,16 @@ class AvoindataDCATAPProfile(RDFProfile):
             g.add((location, DCAT.centroid, Literal(geographical_coverage)))
 
         # dct:accuralPeriodicity
-        update_frequencies = set(u for lang in dataset_dict.get('update_frequency', {}).values() for u in lang if u)
+        update_frequencies = set(u for lang in get_dict(dataset_dict, 'update_frequency').values() for u in lang if u)
 
-        for update_frequency in update_frequencies:
-            g.add((dataset_ref, DCT.accrualPeriodicity, Literal(update_frequency)))
+        if update_frequencies:
+            accrual_periodicity = BNode()
+            g.add((accrual_periodicity, RDF.type, DCT.Frequency))
+            g.add((accrual_periodicity, RDF.value, Literal(', '.join(update_frequencies))))
+            g.add((dataset_ref, DCT.accrualPeriodicity, accrual_periodicity))
 
         # dct:type
-        content_types = set(t for lang in dataset_dict.get('content_type', {}).values() for t in lang if t)
+        content_types = set(t for lang in get_dict(dataset_dict, 'content_type').values() for t in lang if t)
 
         for content_type in content_types:
             concept = Literal(content_type)
@@ -227,3 +273,38 @@ class AvoindataDCATAPProfile(RDFProfile):
         if date_released:
             issued_date = Literal(date_released)
             g.add((dataset_ref, DCT.issued, issued_date))
+
+    def graph_from_catalog(self, catalog_dict, catalog_ref):
+
+        g = self.g
+
+        for prefix, namespace in namespaces.iteritems():
+            g.bind(prefix, namespace)
+
+        g.add((catalog_ref, RDF.type, DCAT.Catalog))
+
+        # Basic fields
+        title = p.config.get('ckan.site_title', '')
+        g.add((catalog_ref, DCT.title, Literal(title)))
+
+        description = p.config.get('ckan.site_description', '')
+        g.add((catalog_ref, DCT.description, Literal(description)))
+
+        homepage = URIRef(p.config.get('ckan.site_url', ''))
+        g.add((homepage, RDF.type, FOAF.Document))
+        g.add((catalog_ref, FOAF.homepage, homepage))
+
+        language = p.config.get('ckan.locale_default', 'en')
+        linguistic_system = URIRef('http://id.loc.gov/vocabulary/iso639-1/%s' % language)
+        g.add((linguistic_system, RDF.type, DCT.LinguisticSystem))
+        g.add((catalog_ref, DCT.language, linguistic_system))
+
+        publisher = URIRef(p.config.get('ckan.site_url', ''))
+        g.add((publisher, RDF.type, FOAF.Agent))
+        g.add((publisher, FOAF.name, Literal(p.config.get('ckan.site_title'))))
+        g.add((catalog_ref, DCT.publisher, publisher))
+
+        # Dates
+        modified = self._last_catalog_modification()
+        if modified:
+            self._add_date_triple(catalog_ref, DCT.modified, modified)
