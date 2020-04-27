710a20f7eccd205eb2019db3a66b731fc02157f0
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-04-27 10:34:53 +0300

    Remove temporary error ignoring

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -5,11 +5,3 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
-
-
-// TODO: Fix errors in third party scripts, ignore them for now
-Cypress.on('uncaught:exception', (err, runnable) => {
-  // returning false here prevents Cypress from
-  // failing the test
-  return false
-})
