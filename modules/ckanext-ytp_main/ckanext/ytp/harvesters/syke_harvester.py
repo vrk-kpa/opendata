5bbf01604ef74fe3fb416b58c4127bd9b4a43f08
Jari Voutilainen <jari.voutilainen@iki.fi>
2020-10-08 13:19:32 +0300

    AV-1285: Map syke metadata

--- modules/ckanext-ytp_main/ckanext/ytp/harvesters/syke_harvester.py
+++ modules/ckanext-ytp_main/ckanext/ytp/harvesters/syke_harvester.py
@@ -1,18 +1,9 @@
 from __future__ import absolute_import
-import six
-import requests
-from requests.exceptions import HTTPError, RequestException
 
 import datetime
-from urllib3.contrib import pyopenssl
-
-from six.moves.urllib.parse import urlencode
-from ckan import model
-from ckan.logic import ValidationError, NotFound, get_action
-from ckan.lib.helpers import json
+from ckan.lib.helpers import json, date_str_to_datetime
 from ckan.plugins import toolkit
-import ckan.lib.plugins as lib_plugins
-
+from ckan.lib.munge import munge_tag
 from ckanext.harvest.model import HarvestObject
 from ckanext.harvest.harvesters.ckanharvester import CKANHarvester
 
@@ -30,34 +21,66 @@ class SYKEHarvester(CKANHarvester):
             'form_config_interface': 'Text'
         }
 
-
     def modify_package_dict(self, package_dict, harvest_object):
 
-        package_dict['notes_translated'] = {
-            "fi": package_dict.get('notes', '')
+        package_dict['title_translated'] = {
+            u"fi": package_dict.get(u'title', u'')
         }
+        package_dict['notes_translated'] = {
+            u"fi": package_dict.get(u'notes', u'')
+        }
+
+        package_dict['collection_type'] = u'Open Data'
+        package_dict['license_id'] = u'cc-by-4.0'
+        package_dict['maintainer_website'] = u'https://www.syke.fi'
+
+        # Remove tags as we don't use them
+        package_dict.pop(u'tags', None)
 
         extras = package_dict.get('extras', [])
 
-        for k, v in [(extra['key'], extra['value']) for extra in extras]:
+        for k, v, i in [(extra['key'], extra['value'], i) for i, extra in enumerate(extras)]:
 
-            if k == 'responsible-party':
+            if k == u'responsible-party':
                 responsible_party = json.loads(v)[0]
-                package_dict['maintainer_email'] = responsible_party.get('email', '')
-        #test = [(k,v) for extra in extras for (k,v) in extra.values()]
-        #log.info(test)
-        #for k, v in [(k, v) for extra in extras for (k, v) in extra.items()]:
-        #    log.info(k)
-        #    log.info(v)
+                package_dict['maintainer'] = responsible_party.get(u'name', u'')
+                package_dict['maintainer_email'] = responsible_party.get(u'email', u'')
 
-        #for extra in extras:
-        #    for key, value in extra.items():
-        #        if extra[key] == 'responsible-party':
-        #            log.info(extra[value])
-        #            responsible_party = json.loads(extra[value])
-        #            log.info(responsible_party)
-        #            package_dict['maintainer_email'] = responsible_party.get('email', '')
+            if k == u'keywords':
+                keywords = [keywords.get(u'keyword', []) for keywords in json.loads(v)]
+                parsed_keywords = [munge_tag(item) for sublist in keywords for item in sublist]
+                package_dict['keywords'] = {
+                    u'fi': parsed_keywords
+                }
 
+                # Remove keywords from extras as same key cannot be in schema and in extras
+                extras.pop(i)
+
+            if k == u'lineage':
+                package_dict['notes_translated']['fi'] += u'\n\n' + v
+
+            if k == u'use_constraints':
+                package_dict['copyright_notice_translated'] = {
+                    u'fi': v
+                }
+
+            if k == u'metadata-date':
+                metadata_date_isoformat = "%s.000000" % date_str_to_datetime(v).isoformat().split('+', 2)[0]
+                package_dict['date_released'] = metadata_date_isoformat
+
+            if k == u'temporal-extent-begin':
+                try:
+                    package_dict['valid_from'] = date_str_to_datetime(v)
+                except ValueError:
+                    log.info("Invalid date for valid_from, ignoring..")
+                    pass
+
+            if k == u'temporal-extent-end':
+                try:
+                    package_dict['valid_until'] = date_str_to_datetime(v)
+                except ValueError:
+                    log.info("Invalid date for valid_until, ignoring..")
+                    pass
 
         return package_dict
 
@@ -97,7 +120,7 @@ class SYKEHarvester(CKANHarvester):
         last_error_free_job = self.last_error_free_job(harvest_job)
         log.debug('Last error-free job: %r', last_error_free_job)
         if (last_error_free_job and
-            not self.config.get('force_all', False)):
+                not self.config.get('force_all', False)):
             get_all_packages = False
 
             # Request only the datasets modified since
@@ -175,233 +198,6 @@ class SYKEHarvester(CKANHarvester):
             return object_ids
         except Exception as e:
             self._save_gather_error('%r' % e.message, harvest_job)
-
-    def fetch_stage(self, harvest_object):
-        # Nothing to do here - we got the package dict in the search in the
-        # gather stage
-        return True
-
-    def import_stage(self, harvest_object):
-        log.debug('In CKANHarvester import_stage')
-
-        base_context = {'model': model, 'session': model.Session,
-                        'user': self._get_user_name()}
-        if not harvest_object:
-            log.error('No harvest object received')
-            return False
-
-        if harvest_object.content is None:
-            self._save_object_error('Empty content for object %s' %
-                                    harvest_object.id,
-                                    harvest_object, 'Import')
-            return False
-
-        self._set_config(harvest_object.job.source.config)
-
-        try:
-            package_dict = json.loads(harvest_object.content)
-
-            if package_dict.get('type') == 'harvest':
-                log.warn('Remote dataset is a harvest source, ignoring...')
-                return True
-
-            # Set default tags if needed
-            default_tags = self.config.get('default_tags', [])
-            if default_tags:
-                if 'tags' not in package_dict:
-                    package_dict['tags'] = []
-                package_dict['tags'].extend(
-                    [t for t in default_tags if t not in package_dict['tags']])
-
-            remote_groups = self.config.get('remote_groups', None)
-            if remote_groups not in ('only_local', 'create'):
-                # Ignore remote groups
-                package_dict.pop('groups', None)
-            else:
-                if 'groups' not in package_dict:
-                    package_dict['groups'] = []
-
-                # check if remote groups exist locally, otherwise remove
-                validated_groups = []
-
-                for group_ in package_dict['groups']:
-                    try:
-                        try:
-                            if 'id' in group_:
-                                data_dict = {'id': group_['id']}
-                                group = get_action('group_show')(base_context.copy(), data_dict)
-                            else:
-                                raise NotFound
-
-                        except NotFound as e:
-                            if 'name' in group_:
-                                data_dict = {'id': group_['name']}
-                                group = get_action('group_show')(base_context.copy(), data_dict)
-                            else:
-                                raise NotFound
-                        # Found local group
-                        validated_groups.append({'id': group['id'], 'name': group['name']})
-
-                    except NotFound as e:
-                        log.info('Group %s is not available', group_)
-                        if remote_groups == 'create':
-                            try:
-                                group = self._get_group(harvest_object.source.url, group_)
-                            except RemoteResourceError:
-                                log.error('Could not get remote group %s', group_)
-                                continue
-
-                            for key in ['packages', 'created', 'users', 'groups', 'tags', 'extras', 'display_name']:
-                                group.pop(key, None)
-
-                            get_action('group_create')(base_context.copy(), group)
-                            log.info('Group %s has been newly created', group_)
-                            validated_groups.append({'id': group['id'], 'name': group['name']})
-
-                package_dict['groups'] = validated_groups
-
-            # Local harvest source organization
-            source_dataset = get_action('package_show')(base_context.copy(), {'id': harvest_object.source.id})
-            local_org = source_dataset.get('owner_org')
-
-            remote_orgs = self.config.get('remote_orgs', None)
-
-            if remote_orgs not in ('only_local', 'create'):
-                # Assign dataset to the source organization
-                package_dict['owner_org'] = local_org
-            else:
-                if 'owner_org' not in package_dict:
-                    package_dict['owner_org'] = None
-
-                # check if remote org exist locally, otherwise remove
-                validated_org = None
-                remote_org = package_dict['owner_org']
-
-                if remote_org:
-                    try:
-                        data_dict = {'id': remote_org}
-                        org = get_action('organization_show')(base_context.copy(), data_dict)
-                        validated_org = org['id']
-                    except NotFound as e:
-                        log.info('Organization %s is not available', remote_org)
-                        if remote_orgs == 'create':
-                            try:
-                                try:
-                                    org = self._get_organization(harvest_object.source.url, remote_org)
-                                except RemoteResourceError:
-                                    # fallback if remote CKAN exposes organizations as groups
-                                    # this especially targets older versions of CKAN
-                                    org = self._get_group(harvest_object.source.url, remote_org)
-
-                                for key in ['packages', 'created', 'users', 'groups', 'tags',
-                                            'extras', 'display_name', 'type']:
-                                    org.pop(key, None)
-                                get_action('organization_create')(base_context.copy(), org)
-                                log.info('Organization %s has been newly created', remote_org)
-                                validated_org = org['id']
-                            except (RemoteResourceError, ValidationError):
-                                log.error('Could not get remote org %s', remote_org)
-
-                package_dict['owner_org'] = validated_org or local_org
-
-            # Set default groups if needed
-            default_groups = self.config.get('default_groups', [])
-            if default_groups:
-                if 'groups' not in package_dict:
-                    package_dict['groups'] = []
-                existing_group_ids = [g['id'] for g in package_dict['groups']]
-                package_dict['groups'].extend(
-                    [g for g in self.config['default_group_dicts']
-                     if g['id'] not in existing_group_ids])
-
-            # Set default extras if needed
-            default_extras = self.config.get('default_extras', {})
-
-            def get_extra(key, package_dict):
-                for extra in package_dict.get('extras', []):
-                    if extra['key'] == key:
-                        return extra
-            if default_extras:
-                override_extras = self.config.get('override_extras', False)
-                if 'extras' not in package_dict:
-                    package_dict['extras'] = []
-                for key, value in default_extras.items():
-                    existing_extra = get_extra(key, package_dict)
-                    if existing_extra and not override_extras:
-                        continue  # no need for the default
-                    if existing_extra:
-                        package_dict['extras'].remove(existing_extra)
-                    # Look for replacement strings
-                    if isinstance(value, six.string_types):
-                        value = value.format(
-                            harvest_source_id=harvest_object.job.source.id,
-                            harvest_source_url=harvest_object.job.source.url.strip('/'),
-                            harvest_source_title=harvest_object.job.source.title,
-                            harvest_job_id=harvest_object.job.id,
-                            harvest_object_id=harvest_object.id,
-                            dataset_id=package_dict['id'])
-
-                    package_dict['extras'].append({'key': key, 'value': value})
-
-            for resource in package_dict.get('resources', []):
-                # Clear remote url_type for resources (eg datastore, upload) as
-                # we are only creating normal resources with links to the
-                # remote ones
-                resource.pop('url_type', None)
-
-                # Clear revision_id as the revision won't exist on this CKAN
-                # and saving it will cause an IntegrityError with the foreign
-                # key.
-                resource.pop('revision_id', None)
-
-            package_dict = self.modify_package_dict(package_dict, harvest_object)
-
-            # validate packages if needed
-            validate_packages = self.config.get('validate_packages', {})
-            if validate_packages:
-                if 'type' not in package_dict:
-                    package_plugin = lib_plugins.lookup_package_plugin()
-                    try:
-                        # use first type as default if user didn't provide type
-                        package_type = package_plugin.package_types()[0]
-                    except (AttributeError, IndexError):
-                        package_type = 'dataset'
-                        # in case a 'dataset' plugin was registered w/o fallback
-                        package_plugin = lib_plugins.lookup_package_plugin(package_type)
-                    package_dict['type'] = package_type
-                else:
-                    package_plugin = lib_plugins.lookup_package_plugin(package_dict['type'])
-
-                errors = {}
-                # if package has been previously imported
-                try:
-                    existing_package_dict = self._find_existing_package(package_dict)
-
-                    if 'metadata_modified' not in package_dict or \
-                        package_dict['metadata_modified'] > existing_package_dict.get('metadata_modified'):
-                        schema = package_plugin.update_package_schema()
-                        data, errors = lib_plugins.plugin_validate(
-                            package_plugin, base_context, package_dict, schema, 'package_update')
-
-                except NotFound:
-
-                    schema = package_plugin.create_package_schema()
-                    data, errors = lib_plugins.plugin_validate(
-                        package_plugin, base_context, package_dict, schema, 'package_create')
-
-                if errors:
-                    raise ValidationError(errors)
-
-            result = self._create_or_update_package(
-                package_dict, harvest_object, package_dict_form='package_show')
-
-            return result
-        except ValidationError as e:
-            self._save_object_error('Invalid package with GUID %s: %r' %
-                                    (harvest_object.guid, e.error_dict),
-                                    harvest_object, 'Import')
-        except Exception as e:
-            self._save_object_error('%s' % e, harvest_object, 'Import')
 
 
 class ContentFetchError(Exception):
