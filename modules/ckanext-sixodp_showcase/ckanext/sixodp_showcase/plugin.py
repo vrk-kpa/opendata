13932fa330d92cf68bb86232bec18ddce54df277
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-02-16 09:45:53 +0200

    AV-1342: Fix breadcrumbs and redirect on update

--- modules/ckanext-sixodp_showcase/ckanext/sixodp_showcase/plugin.py
+++ modules/ckanext-sixodp_showcase/ckanext/sixodp_showcase/plugin.py
@@ -68,8 +68,8 @@ class Sixodp_ShowcasePlugin(ShowcasePlugin):
                       action='delete')
 
         with SubMapper(map, controller='ckanext.showcase.controller:ShowcaseController') as m:
-            # m.connect('ckanext_showcase_read', '/showcase/{id}', action='read',
-            #          ckan_icon='picture')
+            m.connect('ckanext_showcase_read', '/showcase/{id}', action='read',
+                      ckan_icon='picture')
             m.connect('ckanext_showcase_manage_datasets',
                       '/showcase/manage_datasets/{id}',
                       action="manage_datasets", ckan_icon="sitemap")
