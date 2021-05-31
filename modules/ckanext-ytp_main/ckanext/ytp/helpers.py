c9f47acd743d062e0885abe22031a93515125e8e
Teemu Erkkola <teemu.erkkola@gofore.com>
2021-05-31 14:53:19 +0300

    AV-1424: flake8

--- modules/ckanext-ytp_main/ckanext/ytp/helpers.py
+++ modules/ckanext-ytp_main/ckanext/ytp/helpers.py
@@ -634,6 +634,7 @@ def get_field_from_resource_schema(schema, field_name):
     field = next(field for field in schema.get('resource_fields', []) if field.get('field_name') == field_name)
     return field
 
+
 def site_url_with_root_path():
     site_url = toolkit.config.get('ckan.site_url')
     root_path = toolkit.config.get('ckan.root_path')
