44e519e04faa8e0cabe739926ec29c97379f3025
Toni Hämeenniemi <toni.hameenniemi@wirokit.com>
2021-10-26 16:36:19 +0300

    AV-1492: fix ckan extension database init script

--- docker/ckan/scripts/docker-afterinit.d/01_init_extensions.sh
+++ docker/ckan/scripts/docker-afterinit.d/01_init_extensions.sh
@@ -12,13 +12,13 @@ paster --plugin=ckanext-sixodp_showcase sixodp_showcase create_showcase_type_voc
 paster --plugin=ckan db migrate-filestore -c ${APP_DIR}/production.ini
 
 # initialize extension databases
-[[ "${CKAN_PLUGINS}" == *"ytp_request"* ]]  && paster --plugin=ckanext-ytp_request initdb -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"harvest"* ]]      && paster --plugin=ckanext-harvest harvester initdb -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"ytp_spatial"* ]]  && paster --plugin=ckanext-spatial spatial initdb -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"archiver"* ]]     && paster --plugin=ckanext-archiver archiver init -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"qa"* ]]           && paster --plugin=ckanext-qa qa init -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"report"* ]]       && paster --plugin=ckanext-report report initdb -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"matomo"* ]]       && paster --plugin=ckanext-matomo matomo init_db -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"cloudstorage"* ]] && paster --plugin=ckanext-cloudstorage cloudstorage initdb -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"rating"* ]]       && paster --plugin=ckanext-rating rating init -c ${APP_DIR}/production.ini
-[[ "${CKAN_PLUGINS}" == *"reminder"* ]]     && paster --plugin=ckanext-reminder reminder init -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-ytp_request initdb -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-harvest harvester initdb -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-spatial spatial initdb -c ${APP_DIR}/production.ini
+[[ "${CKAN_PLUGINS}" == *" archiver "* ]]     && paster --plugin=ckanext-archiver archiver init -c ${APP_DIR}/production.ini
+[[ "${CKAN_PLUGINS}" == *" qa "* ]]           && paster --plugin=ckanext-qa qa init -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-report report initdb -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-matomo matomo init_db -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-cloudstorage cloudstorage initdb -c ${APP_DIR}/production.ini
+[[ "${CKAN_PLUGINS}" == *" rating "* ]]       && paster --plugin=ckanext-rating rating init -c ${APP_DIR}/production.ini
+paster --plugin=ckanext-reminder reminder init -c ${APP_DIR}/production.ini
