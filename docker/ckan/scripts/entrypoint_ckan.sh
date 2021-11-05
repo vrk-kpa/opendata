5eb71d4296cb7e7b963d3d6d8a0408a12a551c57
Toni Hämeenniemi <toni.hameenniemi@wirokit.com>
2021-11-05 11:05:33 +0200

    AV-1486: simplify ckan docker env variable setup

--- docker/ckan/scripts/entrypoint_ckan.sh
+++ docker/ckan/scripts/entrypoint_ckan.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 set -e
 
+# set env vars for base image prerun.py script
+export CKAN_SQLALCHEMY_URL="postgresql://${DB_CKAN_USER}:${DB_CKAN_PASS}@${DB_HOST}/${DB_CKAN}"
+export CKAN_DATASTORE_WRITE_URL="postgresql://${DB_CKAN_USER}:${DB_CKAN_PASS}@${DB_HOST}/${DB_DATASTORE_READONLY}"
+export CKAN_DATASTORE_READ_URL="postgresql://${DB_DATASTORE_READONLY_USER}:${DB_DATASTORE_READONLY_PASS}@${DB_HOST}/${DB_DATASTORE_READONLY}"
+export CKAN_SOLR_URL="http://${SOLR_HOST}:${SOLR_PORT}/${SOLR_PATH}"
+export CKAN_SYSADMIN_NAME="${SYSADMIN_USER}"
+export CKAN_SYSADMIN_PASSWORD="${SYSADMIN_PASS}"
+export CKAN_SYSADMIN_EMAIL="${SYSADMIN_EMAIL}"
+
 # set the common uwsgi options
 export UWSGI_OPTS="--socket /tmp/uwsgi.sock --uid 92 --gid 92 --http :5000 --master --enable-threads --paste config:/srv/app/production.ini --paste-logger /srv/app/production.ini --lazy-apps --gevent 2000 -p 2 -L --gevent-early-monkey-patch"
 
