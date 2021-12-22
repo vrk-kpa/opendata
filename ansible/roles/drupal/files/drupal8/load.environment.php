d273225ab284de19dd2772f8391c2c30be610e26
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-12-22 12:32:41 +0200

    Update load.environment.php from drupal-project

--- ansible/roles/drupal/files/drupal8/load.environment.php
+++ ansible/roles/drupal/files/drupal8/load.environment.php
@@ -6,15 +6,12 @@
  */
 
 use Dotenv\Dotenv;
-use Dotenv\Exception\InvalidPathException;
 
 /**
  * Load any .env file. See /.env.example.
+ *
+ * Drupal has no official method for loading environment variables and uses
+ * getenv() in some places.
  */
-$dotenv = new Dotenv(__DIR__);
-try {
-  $dotenv->load();
-}
-catch (InvalidPathException $e) {
-  // Do nothing. Production environments rarely use .env files.
-}
+$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->safeLoad();
