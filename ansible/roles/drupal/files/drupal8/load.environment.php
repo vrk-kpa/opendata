a7fece708af3da8cff8fff33654cfff555c077a4
Jari Voutilainen <jari.voutilainen@iki.fi>
2021-12-22 12:45:45 +0200

    Revert "Update load.environment.php from drupal-project"
    This reverts commit d273225ab284de19dd2772f8391c2c30be610e26.

--- ansible/roles/drupal/files/drupal8/load.environment.php
+++ ansible/roles/drupal/files/drupal8/load.environment.php
@@ -6,12 +6,15 @@
  */
 
 use Dotenv\Dotenv;
+use Dotenv\Exception\InvalidPathException;
 
 /**
  * Load any .env file. See /.env.example.
- *
- * Drupal has no official method for loading environment variables and uses
- * getenv() in some places.
  */
-$dotenv = Dotenv::createUnsafeImmutable(__DIR__);
-$dotenv->safeLoad();
+$dotenv = new Dotenv(__DIR__);
+try {
+  $dotenv->load();
+}
+catch (InvalidPathException $e) {
+  // Do nothing. Production environments rarely use .env files.
+}
