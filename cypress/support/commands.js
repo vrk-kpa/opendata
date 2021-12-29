dd33058f6db6cab7f1fdae188dae023110d24ae2
Teemu Erkkola <teemu.erkkola@gofore.com>
2021-12-29 14:31:31 +0200

    AV-1110: Fix cypress tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -296,7 +296,7 @@ Cypress.Commands.add('delete_showcase', (showcase_name) => {
       cy.visit('/data/showcase');
       cy.get('.search-input .search').type(showcase_name + '{enter}');
       cy.get('.showcase-list').should('not.exist');
-      cy.contains("ei löytynyt sovelluksia");
+      cy.contains("Sovelluksia ei löytynyt");
     });
 });
 
