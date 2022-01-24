b133e76765702e198cde42584f6906f2da156699
Pasi Laakso <pasi.laakso@gofore.com>
2022-01-24 11:29:09 +0200

    AV-1580: As guidepage search was removed in new design, remove search related tests from cypress

--- cypress/integration/guide_spec.js
+++ cypress/integration/guide_spec.js
@@ -10,27 +10,10 @@ describe('Guides search functionality', function() {
     cy.visit("/fi/opas");
   })
 
-  it('Visitor uses guide search', function () {
-    cy.get('#guide-search-field').click();
-    cy.get('#guide-search-field').type('helsinki');
-    cy.get('#guide-search-button').click();
-    //cy.visit('http://vagrant.avoindata.fi:9000/data/fi/dataset?q=helsinki')
-  })
-
-
   // With login
   it('Logged in user opens guide front page', function () {
     cy.login_post_request('test-publisher', 'test-publisher');
     cy.visit("/");
     cy.visit("/fi/opas");
   })
-
-  it('Logged in user uses guide search', function () {
-    const options = {scrollBehavior: 'center'};
-    cy.get('#guide-search-field').click();
-    cy.get('#guide-search-field').type('helsinki');
-    cy.get('#guide-search-button').click();
-    //cy.visit('http://vagrant.avoindata.fi:9000/data/fi/dataset?q=helsinki')
-  })
-
 })
