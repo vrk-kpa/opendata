3fa6866a2bd805c33fd656144d5c086968f99a89
Jari Voutilainen <jari.voutilainen@iki.fi>
2022-06-13 10:13:25 +0300

    Update cypress configuration

--- cypress.config.js
+++ cypress.config.js
@@ -8,11 +8,6 @@ module.exports = defineConfig({
     docker: true
   },
   e2e: {
-    // We've imported your old cypress plugins here.
-    // You may want to clean this up later by importing these.
-    setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
-    },
     baseUrl: 'http://localhost',
     specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
   },
