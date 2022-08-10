6a008e55aed73bbabcd9c14b7b2ccad800552569
Ville Teräväinen <ville.teravainen@virnex.fi>
2022-08-10 11:54:57 +0300

    AV-935_cypress_tests

---
+++ cypress/e2e/user_spec.js
@@ -0,0 +1,91 @@
+
+// User registration
+describe('User registration',     {
+    retries: 1
+  }, function() {
+
+    let weakpassword = "1234"
+    let strongpassword = "QWer1234"
+    let newpassword = "Newpass1"
+    //generate random username and email
+    let uuid = require("uuid");
+    let username = uuid.v4();
+    let email = username + "@mail.com" 
+
+    before(function(){
+        //register user
+        cy.register(username, email);
+      })
+
+    this.beforeEach(function(){
+        cy.visit("/");
+    })
+
+    it('Register user and set password', function () {
+        //get the body of the registration email
+        cy.mhGetAllMails().mhFirst().mhGetBody()
+        .then((body) => {
+            // parse body for address
+            var matches = body.match(/\bhttps?:\/\/\S+/gi);
+            if (matches != null){
+                //The first link should be the registration link
+                cy.visit(matches[0]);
+                cy.get('#edit-submit').click();
+
+                cy.get('#edit-pass-pass1').type(weakpassword);
+                cy.get('#edit-pass-pass2').type(weakpassword);
+                cy.get('#edit-submit').click();
+
+                // setting the password should fail
+                cy.get('.alert').should('contain.text', 'The password does not satisfy the password policies.');
+                cy.get('.alert').should('contain.text', 'Password must contain at least 3 types of characters');
+                cy.get('.alert').should('contain.text', 'Password length must be at least 8 characters.');
+            
+                cy.get('#edit-pass-pass1').type(strongpassword);
+                cy.get('#edit-pass-pass2').type(strongpassword);
+                cy.get('#edit-submit').click();
+
+                cy.get('.alert').should('contain.text', 'Muutokset tallennettiin.');
+                cy.get('.form-item-current-pass > .control-label').should('contain.text', 'Nykyinen salasanasi');
+                cy.get('#edit-current-pass').should('exist');
+            }
+            else{
+                //fail test if no urls found
+                throw new Error("Test failed due to no link found in message body");
+            }
+          });
+    })
+
+    it('Change user password', function(){
+        cy.login(username, strongpassword);
+        cy.get('[href="/fi/profile"] > span').click();
+        cy.get('.module-content > :nth-child(1) > :nth-child(2) > a').click();
+        cy.get('#edit-current-pass').type(strongpassword);
+
+        cy.get('#edit-pass-pass1').type(newpassword);
+        cy.get('#edit-pass-pass2').type(newpassword);
+        cy.get('#edit-submit').click();
+        cy.get('.alert').should('contain.text', 'Muutokset tallennettiin.');
+    })
+
+    it('Empty password field doesnt change the password', function(){
+        //use the new password set in the test before
+        cy.login(username, newpassword);
+        cy.get('[href="/fi/profile"] > span').click();
+        cy.get('.module-content > :nth-child(1) > :nth-child(2) > a').click();
+        cy.get('#edit-current-pass').type(newpassword);
+        //leave the two new password fields empty
+        cy.get('#edit-submit').click();
+        cy.get('.alert').should('contain.text', 'Muutokset tallennettiin.');
+
+        //logout and login again
+        // cy.get('[href="/fi/user/logout"] > span').click();
+        cy.logout();
+        cy.get('.header-login-link').click();
+        cy.get('#edit-name').type(username);
+        cy.get('#edit-pass').type(newpassword);
+        cy.get('#edit-submit').click();
+        
+    })
+
+  })
