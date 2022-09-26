522cd2ea8bb107ce9c70538c2c37cf9a5f143b36
Ville Teräväinen <ville.teravainen@virnex.fi>
2022-09-26 14:31:13 +0300

    AV-1797: Refactored tests

--- cypress/e2e/article_spec.js
+++ cypress/e2e/article_spec.js
@@ -1,34 +1,18 @@
-//Articles page search without login to service
-describe('Articles search functionality', function() {
-  // TODO: Haku tehdään sekä kirjautuneena että ilman sitä
-  // TODO: Haun Teko
+describe('Articles page', function() {
 
-  // Without login
-  //it('Articles search basic', function() {
-  it('Visitor opens article list', function () {
+  it("Navigate to article page", function(){
     cy.visit("/");
-    // Articles page
-    cy.visit("/fi/artikkelit");
-  })
-  it('Visitor searches articles', function () {
-    cy.get('#avoindata-articles-search-input').click();
-    cy.get('#avoindata-articles-search-input').type('helsinki');
-    cy.get('#avoindata-articles-search-btn').click();
-  })
+    cy.get('.nav > :nth-child(5) > a').click();
+    cy.location('pathname', {timeout: 60000}).should('contain', `/artikkelit`);
+  });
 
-
-  // With login
-  it('Logged in user opens article list', function () {
-    cy.login_post_request('test-publisher', 'test-publisher');
-    cy.visit("/");
-    // Articles page
+  // This is an old test that should be expanded upon at some point
+  it("Search for an article", function(){
     cy.visit("/fi/artikkelit");
-  })
-  it('Logged in user searches articles', function () {
     const options = {scrollBehavior: 'center'};
     cy.get('#avoindata-articles-search-input').click(options);
     cy.get('#avoindata-articles-search-input').type('helsinki');
     cy.get('#avoindata-articles-search-btn').click(options);
-  })
+  });
 
 })
