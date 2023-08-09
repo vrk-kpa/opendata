b3eea218b0ed75856baf3aee52fdcd3337e38c00
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2023-08-09 10:00:18 +0300

    Do not fail sql commands on first failure

--- cdk/lib/create-databases-and-users.function.ts
+++ cdk/lib/create-databases-and-users.function.ts
@@ -51,38 +51,52 @@ export const handler: Handler = async (event, context) => {
           password: datastoreJobsCredentialObj.password,
           admin: credObj.username
         });
+    } catch (err) {
+      if (err && typeof err === 'object') {
+        console.log(err.toString().replace(/PASSWORD\s(.*;)/, "***"))
+      }
+    }
 
+    try {
       await client.raw("CREATE DATABASE :datastoreJobsDb: OWNER :datastoreUser: ENCODING 'utf-8'; ",
         {
           datastoreJobsDb: "datastore_jobs",
           datastoreUser: datastoreJobsCredentialObj.username
-      });
+        });
+    } catch (err) {
+      if (err && typeof err === 'object') {
+        console.log(err.toString().replace(/PASSWORD\s(.*;)/, "***"))
+      }
+    }
 
+    try {
       await client.raw("GRANT ALL PRIVILEGES ON DATABASE :datastoreJobsDb: TO :datastoreUser:; ",
         {
           datastoreJobsDb: "datastore_jobs",
           datastoreUser: datastoreJobsCredentialObj.username
         })
+    } catch (err) {
+      if (err && typeof err === 'object') {
+        console.log(err.toString().replace(/PASSWORD\s(.*;)/, "***"))
+      }
+    }
 
+    try {
       await client.raw("CREATE DATABASE :datastoreDb: OWNER :datastoreAdmin: ENCODING 'utf-8'; ",
         {
           datastoreDb: 'datastore',
           datastoreAdmin: credObj.username
         })
-
     } catch (err) {
       if (err && typeof err === 'object') {
         console.log(err.toString().replace(/PASSWORD\s(.*;)/, "***"))
       }
-      return {
-        statusCode: 400,
-        body: "something went wrong"
-      }
     }
   }
+
   return {
     statusCode: 200,
     body: "Db and users created"
-  }
 
+  }
 }
