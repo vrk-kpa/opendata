b3ff47d0563584d90fc954fc3be457163ac7949e
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2023-10-04 16:59:32 +0300

    AV-2058: Add subdomain stack for beta environment

---
+++ cdk/lib/sub-domain-stack.ts
@@ -0,0 +1,33 @@
+import {aws_iam, aws_route53, aws_route53_targets, Stack} from "aws-cdk-lib";
+import {Construct} from "constructs";
+import {SubDomainStackProps} from "./sub-domain-stack-props";
+
+export class SubDomainStack extends Stack {
+    readonly subZone: aws_route53.PublicHostedZone;
+    readonly newSubZone: aws_route53.PublicHostedZone;
+    constructor(scope: Construct, id: string, props: SubDomainStackProps) {
+        super(scope, id, props);
+
+        this.subZone = new aws_route53.PublicHostedZone(this, 'SubZone', {
+            zoneName: props.subDomainName + ".avoindata.suomi.fi"
+        })
+
+        const delegationRoleArn = Stack.of(this).formatArn({
+            region: '',
+            service: 'iam',
+            account: props.prodAccountId,
+            resource: 'role',
+            resourceName: 'Route53CrossDelegateRole'
+        })
+
+        const delegationRole = aws_iam.Role.fromRoleArn(this, 'delegationRole', delegationRoleArn)
+
+        new aws_route53.CrossAccountZoneDelegationRecord(this, 'delegate', {
+            delegatedZone: this.subZone,
+            delegationRole: delegationRole,
+            parentHostedZoneName: "avoindata.suomi.fi"
+        })
+
+    }
+
+}
