2d8d028f6a60f5fa3b6ec56648a447466520453e
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2024-01-18 10:46:47 +0200

    AV-2038: Protect cloudfront instead of load balancer

--- cdk/lib/shield-stack-props.ts
+++ cdk/lib/shield-stack-props.ts
@@ -1,8 +1,6 @@
-import {aws_cloudfront, aws_elasticloadbalancingv2} from "aws-cdk-lib";
 import {CommonStackProps} from "./common-stack-props";
 
 export interface ShieldStackProps extends CommonStackProps{
-  loadBalancer: aws_elasticloadbalancingv2.IApplicationLoadBalancer,
   bannedIpsRequestSamplingEnabled: boolean,
   requestSampleAllTrafficEnabled: boolean,
   highPriorityRequestSamplingEnabled: boolean,
