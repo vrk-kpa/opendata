b033340c258fc386877bd1544d4655b5ffbff67d
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2025-02-17 10:51:37 +0200

    Allow cloudstorage to use test role in aws

--- cdk/lib/ci-test-stack-props.ts
+++ cdk/lib/ci-test-stack-props.ts
@@ -3,5 +3,6 @@ import {StackProps} from "aws-cdk-lib";
 export interface CiTestStackProps extends StackProps {
   testBucketName: string,
   githubOrg: string,
-  githubRepo: string
+  githubRepo: string,
+  githubRepo2: string
 }
