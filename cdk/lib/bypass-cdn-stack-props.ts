a52b2c88c2df4824eb12d84b6837c7088b4c218a
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2025-05-06 14:08:47 +0300

    AV-2250: Cleanup cloudfront and related stacks

--- cdk/lib/bypass-cdn-stack-props.ts
+++
@@ -1,7 +0,0 @@
-import {aws_route53} from "aws-cdk-lib";
-import * as elb from "aws-cdk-lib/aws-elasticloadbalancingv2";
-import {CommonStackProps} from "./common-stack-props";
-
-export interface BypassCdnStackProps extends CommonStackProps {
-  loadbalancer: elb.IApplicationLoadBalancer
-}
