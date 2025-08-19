b6e8f0049a76fa4d08c3fb7d55eba412e37b1888
Teemu Erkkola <teemu.erkkola@gofore.com>
2025-08-19 12:42:56 +0300

    Convert CKAN prerun.py to connection_check.py, do db init in init script

--- ckan/scripts/reinit_ckan.sh
+++ ckan/scripts/reinit_ckan.sh
@@ -10,7 +10,10 @@ jinja2 ${TEMPLATE_DIR}/ckan-uwsgi.ini.j2 -o ${APP_DIR}/ckan-uwsgi.ini
 jinja2 ${TEMPLATE_DIR}/datastore_permissions.sql.j2 -o ${SCRIPT_DIR}/datastore_permissions.sql
 
 # run prerun script that checks connections and inits db
-python prerun.py || { echo '[CKAN prerun] FAILED. Exiting...' ; exit 1; }
+python connection_check.py || { echo '[CKAN connection check] FAILED. Exiting...' ; exit 1; }
+
+echo "Init CKAN database ..."
+ckan -c ${APP_DIR}/production.ini db init
 
 echo "Upgrade CKAN database ..."
 ckan -c ${APP_DIR}/production.ini db upgrade
