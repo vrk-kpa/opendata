326e84f4e004ceb77292b19d88189eab33f1f356
Teemu Erkkola <teemu.erkkola@gofore.com>
2026-02-05 09:16:15 +0200

    AV-2472: Add missing file

---
+++ ckan/ckanext/ckanext-ytp_main/ckanext/ytp/tests/factories.py
@@ -0,0 +1,13 @@
+import factory
+
+from ckan.tests.factories import Organization
+
+from faker import Faker
+
+fake = Faker()
+
+
+class OpendataOrganization(Organization):
+    title_translated = {
+        'fi': fake.sentence(nb_words=5)
+    }
