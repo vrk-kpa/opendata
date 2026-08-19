82b91e002e6ec7d3cf070f5640099c0fd7235770
Teemu Erkkola <teemu.erkkola@gofore.com>
2026-08-19 13:14:40 +0300

    AV-2557: Reset DB after each apiset test again, because otherwise test retries may fail to already existing datasets

--- cypress/e2e/apiset_spec.js
+++ cypress/e2e/apiset_spec.js
@@ -264,7 +264,7 @@ describe('Apiset tests',
   })
 
   describe('Apiset creation', function () {
-    before(function () {
+    beforeEach(function () {
       cy.reset_db();
       cy.ensure_user_is_in_ckan("test-user", "test-user")
       cy.perform_ckan_actions(ckan => {
@@ -275,8 +275,6 @@ describe('Apiset tests',
           }]
         })
       })
-    });
-    beforeEach(function () {
       cy.login_post_request('test-user', 'test-user')
       cy.visit('/');
     });
