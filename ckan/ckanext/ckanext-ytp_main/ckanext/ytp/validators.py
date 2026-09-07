d4573d4464dada412bf9690a58dbe8e42771dbc5
Jari Voutilainen <jari-pekka.voutilainen@gofore.com>
2026-09-07 13:18:21 +0300

    Check for none as fluent_text might pass none

--- ckan/ckanext/ckanext-ytp_main/ckanext/ytp/validators.py
+++ ckan/ckanext/ckanext-ytp_main/ckanext/ytp/validators.py
@@ -618,12 +618,14 @@ def admin_only_field(field, schema):
 @scheming_validator
 def use_url_for_name_if_left_empty(field, schema):
     def validator(key, data, errors, context):
-        resource_names_translated = json.loads(data.get(key, ''))
-        resource_url = data.get(key[:-1] + ('url',), '')
+        value = data[key]
+        if value is not None:
+            resource_names_translated = json.loads(value)
+            resource_url = data.get(key[:-1] + ('url',), '')
 
-        if resource_names_translated.get('fi', '') == '' and resource_url != '':
-            resource_names_translated['fi'] = resource_url
-            data[key] = json.dumps(resource_names_translated)
+            if resource_names_translated.get('fi', '') == '' and resource_url != '':
+                resource_names_translated['fi'] = resource_url
+                data[key] = json.dumps(resource_names_translated)
     return validator
 
 
